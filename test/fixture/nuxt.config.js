65cacfdeeb20833516998ec03379679c049fa5d8
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2018-01-27 10:46:42 -0500

    feat: Add redirectRootToLocale option
    Provides redirectRootToLocale option to redirect / to a specific locale if noPrefixDefaultLocale is
    disabled

--- test/fixture/nuxt.config.js
+++ test/fixture/nuxt.config.js
@@ -19,6 +19,8 @@ module.exports = {
         }
       ],
       defaultLocale: 'en',
+      noPrefixDefaultLocale: true,
+      redirectRootToLocale: 'en',
       vueI18n: {
         messages: {
           fr: {
