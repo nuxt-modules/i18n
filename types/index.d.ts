63831cd960b215c895323cf12b1f119e02cc0609
Kevin <contact@kevinmarrec.com>
2018-11-18 16:16:53 +0100

    Change Architecture/CodeStyle of types + Test types

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,10 +1,2 @@
-import Vue from 'vue'
-import { Route } from 'vue-router'
-
-declare module 'vue/types/vue' {
-  interface Vue {
-    localePath(route: string | Route, locale?: string): string
-    switchLocalePath(locale: string): string
-    getRouteBaseName(route: Route): string
-  }
-}
+// augment typings of Vue.js
+import "./vue";
---
+++ types/vue.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Extends interfaces in Vue.js
+ */
+
+import Vue from "vue";
+import { Route } from "vue-router";
+
+declare module "vue/types/vue" {
+  interface Vue {
+    localePath(route: string | Route, locale?: string): string;
+    switchLocalePath(locale: string): string;
+    getRouteBaseName(route: Route): string;
+  }
+}
