5c949b767756ea5dfe07e52070be597a634c192a
richie3366 <richie3366@gmail.com>
2018-12-12 14:13:58 +0100

    Enabled secure cookie param

--- src/plugins/main.js
+++ src/plugins/main.js
@@ -84,12 +84,14 @@ export default async ({ app, route, store, req, res }) => {
     const date = new Date()
     if (res && res.setHeader) {
       const redirectCookie = cookie.serialize(key, val, {
-        expires: new Date(date.setDate(date.getDate() + 365))
+        expires: new Date(date.setDate(date.getDate() + 365)),
+        secure: true
       })
       res.setHeader('Set-Cookie', redirectCookie)
     } else {
       Cookies.set(key, val, {
-        expires: new Date(date.setDate(date.getDate() + 365))
+        expires: new Date(date.setDate(date.getDate() + 365)),
+        secure: true
       })
     }
   }
