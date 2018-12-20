42c25ba1dd620a22149db4d0c9c5f000cb097e2c
Alexander Canals <spam@canals.ch>
2018-12-20 22:28:27 +0100

    Make result const

--- src/templates/utils.js
+++ src/templates/utils.js
@@ -20,7 +20,7 @@ export async function loadLanguageAsync (i18n, locale) {
         try {
           const module = await import(/* webpackChunkName: "lang-[request]" */ '~/<%= options.langDir %>' + file)
           const messages = module.default ? module.default : module
-          let result = typeof messages === 'function' ? await Promise.resolve(messages()) : messages
+          const result = typeof messages === 'function' ? await Promise.resolve(messages()) : messages
           i18n.setLocaleMessage(locale, result)
           i18n.loadedLanguages.push(locale)
           return result
