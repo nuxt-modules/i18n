225e700dd0bfa18faa506c91cb44db114e2d7d90
Илья <iliyazelenkog@gmail.com>
2019-01-14 16:35:30 +0200

    fix: Update types (#167)

--- types/vue.d.ts
+++ types/vue.d.ts
@@ -3,12 +3,12 @@
  */
 
 import Vue from "vue";
-import { Route } from "vue-router";
+import { RawLocation } from "vue-router";
 
 declare module "vue/types/vue" {
   interface Vue {
-    localePath(route: string | Route, locale?: string): string;
+    localePath(route: RawLocation, locale?: string): string;
     switchLocalePath(locale: string): string;
-    getRouteBaseName(route: Route): string;
+    getRouteBaseName(route: RawLocation): string;
   }
 }
