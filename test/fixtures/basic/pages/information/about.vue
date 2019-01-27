487f610be959cb4d7909d779b8994dfbf4397825
Paul Gascou-Vaillancourt <paul.gascvail@gmail.com>
2019-01-27 10:20:11 -0500

    test: Add test case for non-root path customization

---
+++ test/fixtures/basic/pages/information/about.vue
@@ -0,0 +1,24 @@
+<template>
+<div>
+  <LangSwitcher />
+  {{ $t('about') }}
+  <nuxt-link exact :to="localePath('index')">{{ $t('home') }}</nuxt-link>
+</div>
+</template>
+
+<script>
+import LangSwitcher from '../../components/LangSwitcher'
+
+export default {
+  components: {
+    LangSwitcher
+  },
+  nuxtI18n: {
+    paths: {
+      en: '/about-us', // -> accessible at /about-us (no prefix since it's the default locale)
+      fr: 'a-propos',  // -> accessible at /fr/information/a-propos
+      es: '/sobre'     // -> accessible at /es/sobre
+    }
+  }
+}
+</script>
