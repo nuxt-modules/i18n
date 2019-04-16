1fd9274c1ee54d7478e6f9ec4d815a0d21e9dedb
darcoder <falcon@starline.ee>
2019-04-16 11:40:17 +0300

    just pulls out the file COMPONENT_OPTIONS_KEY
    https://nuxt-community.github.io/nuxt-i18n/routing.html#custom-paths

--- src/helpers/components.js
+++ src/helpers/components.js
@@ -1,8 +1,8 @@
 const { readFileSync } = require('fs')
 const { COMPONENT_OPTIONS_KEY } = require('./constants')
 
-const acorn = require('acorn')
-const walker = require('acorn-walk')
+//const acorn = require('acorn')
+//const walker = require('acorn-walk')
 const compiler = require('vue-template-compiler')
 
 exports.extractComponentOptions = (path) => {
@@ -13,6 +13,31 @@ exports.extractComponentOptions = (path) => {
   }
 
   const script = Component.script.content
+
+  let pos = script.indexOf(COMPONENT_OPTIONS_KEY)
+  let data_str = ''
+  let go = false
+  let chk = 0
+  let sym = ''
+
+  if (pos !== -1) {
+    for (step = pos; step < script.length; step++) {
+      sym = script.charAt(step)
+      if (sym==='{') {
+        go = true;
+        chk++;
+      }
+      if (sym==='}') chk--;
+      data_str += script.charAt(step)
+      if (go && !chk) break;
+    }
+  }
+  
+  if (data_str.length>0) {
+    componentOptions =  eval(`({${data_str}})`)[COMPONENT_OPTIONS_KEY]
+  }
+
+  /*  
   const parsed = acorn.parse(script, {
     ecmaVersion: 10,
     sourceType: 'module'
@@ -25,6 +50,7 @@ exports.extractComponentOptions = (path) => {
       }
     }
   })
-
+  */
+  
   return componentOptions
 }
