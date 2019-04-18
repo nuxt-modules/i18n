10d653cd37821e329feedc82b1f40bc3ae144770
falcon <falcon@madein.ee>
2019-04-18 12:33:30 +0300

    remove acorn
    only one COMPONENT_OPTIONS_KEY in file

--- src/helpers/components.js
+++ src/helpers/components.js
@@ -2,8 +2,8 @@ const { readFileSync } = require('fs')
 const { COMPONENT_OPTIONS_KEY } = require('./constants')
 
 const acorn = require('acorn')
-const walker = require('acorn-walk')
-const compiler = require('vue-template-compiler')
+// const walker = require('acorn-walk')
+// const compiler = require('vue-template-compiler')
 
 exports.extractComponentOptions = (path) => {
   let componentOptions = {}
@@ -13,7 +13,38 @@ exports.extractComponentOptions = (path) => {
   }
 
   const script = Component.script.content
-  const parsed = acorn.parse(script, {
+
+  let pos = script.indexOf(COMPONENT_OPTIONS_KEY)
+
+  if (pos !== -1) {
+    let dataStr = ''
+    let go = false
+
+    let chk = 0
+    let sym = ''
+
+    for (step = pos; step < script.length; step++) {
+      sym = script.charAt(step)
+      if (sym === '{') {
+        go = true
+        chk++
+      }
+      if (sym === '}') chk--
+      dataStr += sym
+
+      if (!go && (sym === '\n' || sym === '\r')) {
+        break
+      }
+
+      if (go && !chk) break
+
+    }
+
+    if (dataStr.length > 0) {
+      componentOptions = eval(`({${dataStr}})`)[COMPONENT_OPTIONS_KEY]
+    }
+  }
+/*  const parsed = acorn.parse(script, {
     ecmaVersion: 10,
     sourceType: 'module'
   })
@@ -25,6 +56,6 @@ exports.extractComponentOptions = (path) => {
       }
     }
   })
-
+  */
   return componentOptions
 }
