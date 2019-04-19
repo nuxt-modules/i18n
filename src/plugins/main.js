026d7cc5566579656bb7ed9a09702a71decdb045
orblazer <orblazer.minecraft@gmail.com>
2019-04-19 16:47:02 +0200

    fix: lint

--- src/plugins/main.js
+++ src/plugins/main.js
@@ -90,7 +90,7 @@ export default async ({ app, route, store, req }) => {
     const { loadLanguageAsync } = require('./utils')
 
     // Load fallback lang
-    if(app.i18n.fallbackLocale && app.i18n.locale !== app.i18n.fallbackLocale) {
+    if (app.i18n.fallbackLocale && app.i18n.locale !== app.i18n.fallbackLocale) {
       await loadLanguageAsync(app.i18n, app.i18n.fallbackLocale)
     }
 
