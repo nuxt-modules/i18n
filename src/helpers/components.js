74b47b87d82540cd6bf5dd147f48d8c9d1a52c72
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2019-06-02 20:11:38 +0200

    fix(format): removed semicolons

--- src/helpers/components.js
+++ src/helpers/components.js
@@ -2,9 +2,9 @@ const { readFileSync } = require('fs')
 const { COMPONENT_OPTIONS_KEY } = require('./constants')
 
 const acorn = require('acorn')
-const dynamicImport = require('acorn-dynamic-import');
-const inject = require('acorn-dynamic-import/lib/walk');
-const walker = inject.default(require('acorn-walk'));
+const dynamicImport = require('acorn-dynamic-import')
+const inject = require('acorn-dynamic-import/lib/walk')
+const walker = inject.default(require('acorn-walk'))
 // Must not be an explicit dependency to avoid version mismatch issue.
 // See https://github.com/nuxt-community/nuxt-i18n/issues/297
 const compiler = require('vue-template-compiler')
