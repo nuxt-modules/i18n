0e90c4b17afa27e1b94a7921319eb477cf3635b3
Stephan Gerbeth <stephan.gerbeth@gmail.com>
2019-06-02 20:06:57 +0200

    fix(acorn): implemented acorn-dynamic-import

--- src/helpers/components.js
+++ src/helpers/components.js
@@ -2,7 +2,9 @@ const { readFileSync } = require('fs')
 const { COMPONENT_OPTIONS_KEY } = require('./constants')
 
 const acorn = require('acorn')
-const walker = require('acorn-walk')
+const dynamicImport = require('acorn-dynamic-import');
+const inject = require('acorn-dynamic-import/lib/walk');
+const walker = inject.default(require('acorn-walk'));
 // Must not be an explicit dependency to avoid version mismatch issue.
 // See https://github.com/nuxt-community/nuxt-i18n/issues/297
 const compiler = require('vue-template-compiler')
@@ -15,18 +17,18 @@ exports.extractComponentOptions = (path) => {
   }
 
   const script = Component.script.content
-  const parsed = acorn.parse(script, {
+  const parsed = acorn.Parser.extend(dynamicImport.default).parse(script, {
     ecmaVersion: 10,
     sourceType: 'module'
   })
   walker.simple(parsed, {
-    Property (node) {
+    Property(node) {
       if (node.key.name === COMPONENT_OPTIONS_KEY) {
         const data = script.substring(node.start, node.end)
         componentOptions = eval(`({${data}})`)[COMPONENT_OPTIONS_KEY] // eslint-disable-line
       }
     }
-  })
+  }, walker.base)
 
   return componentOptions
 }
