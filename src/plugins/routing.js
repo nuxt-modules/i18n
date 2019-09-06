c4186f8e33264fcfa65f35206acfb193d7ba0f97
Jake Engel <jengel@tymail.com>
2019-09-06 13:12:53 -0500

    Resolve path route using vue-router

--- src/plugins/routing.js
+++ src/plugins/routing.js
@@ -27,31 +27,42 @@ function localePathFactory (i18nPath, routerPath) {
       route = { name: route }
     }
 
-    // if route has a path defined but no route name, generate path without route
+    let localizedRoute
+    const router = this[routerPath]
+
     if (route.path && !route.name) {
-      // prepend locale if current is not default and not using different domains
-      const appendPath = !(locale === defaultLocale && strategy === STRATEGIES.PREFIX_EXCEPT_DEFAULT) && !this[i18nPath].differentDomains
-      const fullPath = (appendPath ? `/${locale}${route.path}` : route.path)
-      return fullPath
-    }
+      // if route has a path defined but no name, resolve full route using the path
+      const isPrefixed = (
+        // don't prefix default locale
+        !(locale === defaultLocale && STRATEGY === STRATEGIES.PREFIX_EXCEPT_DEFAULT) &&
+        // no prefix for any language
+        !(STRATEGY === STRATEGIES.NO_REFIX) &&
+        // no prefix for different domains
+        !this[i18nPath].differentDomains
+      )
 
-    // Build localized route options
-    let name = route.name + (STRATEGY === STRATEGIES.NO_PREFIX ? '' : routesNameSeparator + locale)
+      const path = (isPrefixed ? `/${locale}${route.path}` : route.path)
 
-    // Match route without prefix for default locale
-    if (locale === defaultLocale && STRATEGY === STRATEGIES.PREFIX_AND_DEFAULT) {
-      name += routesNameSeparator + defaultLocaleRouteNameSuffix
-    }
+      localizedRoute = Object.assign({}, route, { path })
+    } else {
+      // otherwise resolve route via the route name
+      // Build localized route options
+      let name = route.name + (STRATEGY === STRATEGIES.NO_PREFIX ? '' : routesNameSeparator + locale)
 
-    const localizedRoute = Object.assign({}, route, { name })
+      // Match route without prefix for default locale
+      if (locale === defaultLocale && STRATEGY === STRATEGIES.PREFIX_AND_DEFAULT) {
+        name += routesNameSeparator + defaultLocaleRouteNameSuffix
+      }
 
-    const { params } = localizedRoute
-    if (params && params['0'] === undefined && params.pathMatch) {
-      params['0'] = params.pathMatch
+      localizedRoute = Object.assign({}, route, { name })
+
+      const { params } = localizedRoute
+      if (params && params['0'] === undefined && params.pathMatch) {
+        params['0'] = params.pathMatch
+      }
     }
 
     // Resolve localized route
-    const router = this[routerPath]
     const { route: { fullPath } } = router.resolve(localizedRoute)
     return fullPath
   }
