10c1d9dc4fad587de64fbec27254692b7cf73154
Rafał Chłodnicki <rchl2k@gmail.com>
2019-09-25 23:10:46 +0200

    fix: Locale prefixes missing for child routes with custom paths
    If one assigned (in component options or pages object) custom, absolute
    paths for routes and those routes were child routes, then those routes
    didn't get the locale prefix assigned as prefix was never added to
    child routes.
    
    That wasn't a problem if route was a top-level route or no custom paths
    were assigned as then the path was relative and didn't need locale
    prefix to be added.
    
    Fix by also adding locale prefix for routes that are child routes with
    absolute path.
    
    Resolves #359

--- test/fixture/basic/pages/posts/index.vue
+++ test/fixture/basic/pages/posts/index.vue
@@ -1,19 +1,19 @@
 <template>
-<div>
-  <nuxt-link
-    exact
-    :to="localePath({
-      name: 'posts-slug',
-      params: {
-        slug: params[$i18n.locale].slug
-      }
-    })">{{ params[$i18n.locale].slug }}</nuxt-link>
-</div>
+  <div>
+    <nuxt-link
+      exact
+      :to="localePath({
+        name: 'posts-slug',
+        params: {
+          slug: params[$i18n.locale].slug
+        }
+      })">{{ params[$i18n.locale].slug }}</nuxt-link>
+  </div>
 </template>
 
 <script>
 export default {
-  async asyncData () {
+  data () {
     return {
       params: {
         en: { slug: 'my-post' },
