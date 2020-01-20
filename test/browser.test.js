2a260fa21de1917f250517aa0da8783cb4892fe9
Rafał Chłodnicki <rchl2k@gmail.com>
2020-01-20 14:26:21 +0100

    chore(tests): Make tests compatible with Nuxt 2.11
    Required due to router.push shim introduced in
    https://github.com/nuxt/nuxt.js/pull/6722

--- test/browser.test.js
+++ test/browser.test.js
@@ -10,7 +10,11 @@ const createNavigator = page => {
     // When returning value resolved by `push`, `chrome/selenium`` crashes with:
     // WebDriverError: unknown error: unhandled inspector error: {"code":-32000,"message":"Object reference chain is too long"}
     // Chain and return nothing to work around.
-    await page.runAsyncScript(path => window.$nuxt.$router.push(path).then(() => {}), path)
+    await page.runAsyncScript(path => {
+      return new Promise((resolve, reject) => {
+        window.$nuxt.$router.push(path, resolve, reject)
+      })
+    }, path)
     await new Promise(resolve => setTimeout(resolve, 50))
   }
 }
