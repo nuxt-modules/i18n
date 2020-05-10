bb0b9113bbb715ed73dda93134ebf11ad2f0500c
Rafał Chłodnicki <rchl2k@gmail.com>
2020-05-10 20:32:35 +0200

    chore(lint): fix lint issues in eslintrc.js (doh)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,15 @@
 module.exports = {
   root: true,
   extends: [
-    '@nuxtjs/eslint-config-typescript',
+    '@nuxtjs/eslint-config-typescript'
   ],
   plugins: [
-    'jest',
+    'jest'
   ],
   rules: {
     'arrow-parens': 'off',
     'vue/html-closing-bracket-newline': 'off',
     'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
-  },
+    'vue/singleline-html-element-content-newline': 'off'
+  }
 }
