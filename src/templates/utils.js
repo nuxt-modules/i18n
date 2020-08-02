2b28702451978628a0391285ddff0b0bae748c60
Rafał Chłodnicki <rchl2k@gmail.com>
2020-08-02 22:05:25 +0200

    fix(lazy): include default locale in the main bundle
    This avoid extra request for the locale that is always needed anyway.
    
    Resolves #818

--- src/templates/utils.js
+++ src/templates/utils.js
@@ -1,8 +1,12 @@
 import {
   LOCALE_CODE_KEY,
   LOCALE_FILE_KEY,
-  MODULE_NAME
+  MODULE_NAME,
+  defaultLangFile
 } from './options'
+/* <% if (options.defaultLangFile) { %> */
+import defaultLangModule from './default-lang/<%= options.defaultLangFile %>'
+/* <% } %> */
 
 /**
  * Asynchronously load messages from translation files
@@ -17,15 +21,20 @@ export async function loadLanguageAsync (context, locale) {
   }
 
   if (!app.i18n.loadedLanguages.includes(locale)) {
-    const langOptions = app.i18n.locales.find(l => l[LOCALE_CODE_KEY] === locale)
-    if (langOptions) {
-      const file = langOptions[LOCALE_FILE_KEY]
+    const localeObject = app.i18n.locales.find(l => l[LOCALE_CODE_KEY] === locale)
+    if (localeObject) {
+      const file = localeObject[LOCALE_FILE_KEY]
       if (file) {
         // Hiding template directives from eslint so that parsing doesn't break.
         /* <% if (options.langDir) { %> */
         try {
-          const module = await import(/* webpackChunkName: "lang-[request]" */ '~/<%= options.langDir %>' + file)
-          const messages = module.default ? module.default : module
+          let langFileModule
+          if (file === defaultLangFile) {
+            langFileModule = defaultLangModule
+          } else {
+            langFileModule = await import(/* webpackChunkName: "lang-[request]" */ `./langs/${file}`)
+          }
+          const messages = langFileModule.default || langFileModule
           const result = typeof messages === 'function' ? await Promise.resolve(messages(context, locale)) : messages
           app.i18n.setLocaleMessage(locale, result)
           app.i18n.loadedLanguages.push(locale)
