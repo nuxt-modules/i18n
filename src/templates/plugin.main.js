1110aafd4f0f818f724226b34065c05ea37a8ee4
Sebastian Landwehr <sebastianlandw@gmail.com>
2020-09-19 23:52:03 +0200

    give route locale priority over browser detection

--- src/templates/plugin.main.js
+++ src/templates/plugin.main.js
@@ -185,6 +185,11 @@ export default async (context) => {
       return false
     }
 
+    const routeLocale = getLocaleFromRoute(route)
+    if (routeLocale !== null) {
+      return routeLocale
+    }
+
     if (onlyOnRoot && strategy !== STRATEGIES.NO_PREFIX && route.path !== '/') {
       return false
     }
@@ -258,9 +263,6 @@ export default async (context) => {
       const options = { localDomainKey: LOCALE_DOMAIN_KEY, localeCodeKey: LOCALE_CODE_KEY }
       const domainLocale = getLocaleDomain(locales, req, options)
       finalLocale = domainLocale
-    } else if (strategy !== STRATEGIES.NO_PREFIX) {
-      const routeLocale = getLocaleFromRoute(route)
-      finalLocale = routeLocale
     } else if (useCookie) {
       finalLocale = app.i18n.getLocaleCookie()
     }
