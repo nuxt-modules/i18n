1af1ab208ec9457eae63e878cd976c01b261e9ac
Michał Kędrzyński <m.kedrzynski@perfectgym.com>
2021-03-08 14:01:19 +0100

    fix: langDir path must be escaped on windows

--- src/templates/options.js
+++ src/templates/options.js
@@ -1,8 +1,6 @@
 <%
 function stringifyValue(value) {
-  if (typeof value === 'string') {
-    return `'${value}'`
-  } else if (value === undefined || value === null || typeof value === 'boolean' || typeof value === 'function') {
+  if (value === undefined || value === null || typeof value === 'boolean' || typeof value === 'function') {
     return String(value);
   } else {
     return JSON.stringify(value)
