20452b5716061968add931e7ee9ccd00d1f44538
Johann Schopplich <mail@johannschopplich.com>
2022-02-14 10:29:53 +0100

    fix: vue-i18n runtime path (#1)

--- packages/nuxt-i18n/src/nuxt3.ts
+++ packages/nuxt-i18n/src/nuxt3.ts
@@ -8,7 +8,7 @@ export async function setupNuxt3(nuxt: Nuxt) {
   // resolve vue-i18n
   const vueI18nPath = nuxt.options.dev
     ? 'vue-i18n/dist/vue-i18n.esm-bundler.js'
-    : 'vue-i18n/dist/vue-i18n-runtime.esm-bundler.js'
+    : 'vue-i18n/dist/vue-i18n.runtime.esm-bundler.js'
   nuxt.options.alias['vue-i18n'] = resolveModule(vueI18nPath, {
     paths: nuxt.options.modulesDir
   })
