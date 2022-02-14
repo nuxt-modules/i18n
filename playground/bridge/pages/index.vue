c86abc8aa8bbacc18546c2e005ac040d6d7c77f2
kazuya kawaguchi <kawakazu80@gmail.com>
2022-02-14 10:51:54 +0900

    update playgrounds

--- playground/bridge/pages/index.vue
+++ playground/bridge/pages/index.vue
@@ -2,11 +2,8 @@
 import { useI18n } from 'vue-i18n-bridge'
 import { localePath, switchLocalePath, useI18nHead } from 'vue-i18n-routing'
 
-const { t, locale, locales, localeCodes } = useI18n()
+const { t, locale, locales } = useI18n()
 const i18nHead = useI18nHead({ addSeoAttributes: true })
-console.log('i18nHead htmlAttrs', JSON.stringify(i18nHead.value.htmlAttrs))
-console.log('i18nHead link', i18nHead.value.link)
-console.log('i18nHead meta', i18nHead.value.meta)
 
 useMeta({
   title: computed(() => t('hello', { name: 'nuxt bridge' })),
@@ -16,6 +13,12 @@ useMeta({
   link: computed(() => [...(i18nHead.value.link || [])]),
   meta: computed(() => [...(i18nHead.value.meta || [])])
 })
+
+function getLocaleName(code) {
+  const locale = locales.value.find(i => i.code === code)
+  return locale ? locale.name : code
+}
+
 const availableLocales = computed(() => {
   return locales.value.filter(i => i.code !== locale.value)
 })
@@ -23,21 +26,23 @@ const availableLocales = computed(() => {
 
 <template>
   <div>
-    <h1>Nuxt Bridge</h1>
+    <h1>Demo: Nuxt Bridge</h1>
     <h2>{{ $t('hello', { name: 'nuxt bridge' }) }}</h2>
+    <h2>Pages</h2>
     <nav>
-      <NuxtLink :to="localePath('/')">Home</NuxtLink>
+      <NuxtLink :to="localePath('/')">Home</NuxtLink> |
       <NuxtLink :to="localePath('/about')">About</NuxtLink>
     </nav>
-    <form>
-      <select id="locale-select" v-model="locale">
-        <template v-for="code in localeCodes">
-          <option :value="code">{{ code }}</option>
-        </template>
-      </select>
-    </form>
-    <span v-for="locale in availableLocales" :key="locale.code">
-      <NuxtLink :to="switchLocalePath(locale.code) || ''">{{ locale.name }}</NuxtLink> |
-    </span>
+    <h2>Current Language: {{ getLocaleName(locale) }}</h2>
+    <h2>Select Languages</h2>
+    <nav>
+      <span v-for="locale in availableLocales" :key="locale.code">
+        <NuxtLink :to="switchLocalePath(locale.code) || ''">{{ locale.name }}</NuxtLink> |
+      </span>
+    </nav>
+    <h2>I18n Head</h2>
+    <div>
+      <p>{{ i18nHead }}</p>
+    </div>
   </div>
 </template>
--- playground/nuxt3/pages/index.vue
+++ playground/nuxt3/pages/index.vue
@@ -2,7 +2,7 @@
 import { useI18n } from 'vue-i18n'
 import { localePath, switchLocalePath, useI18nHead } from 'vue-i18n-routing'
 
-const { t, locale, locales, localeCodes } = useI18n()
+const { t, locale, locales } = useI18n()
 
 const i18nHead = useI18nHead({ addSeoAttributes: true })
 useMeta({
@@ -14,6 +14,11 @@ useMeta({
   meta: computed(() => [...(i18nHead.value.meta || [])])
 })
 
+function getLocaleName(code) {
+  const locale = locales.value.find(i => i.code === code)
+  return locale ? locale.name : code
+}
+
 const availableLocales = computed(() => {
   return locales.value.filter(i => i.code !== locale.value)
 })
@@ -21,21 +26,23 @@ const availableLocales = computed(() => {
 
 <template>
   <div>
-    <h1>Nuxt 3</h1>
+    <h1>Demo: Nuxt 3</h1>
     <h2>{{ $t('hello', { name: 'nuxt3' }) }}</h2>
+    <h2>Pages</h2>
     <nav>
-      <NuxtLink :to="localePath('/')">Home</NuxtLink>
+      <NuxtLink :to="localePath('/')">Home</NuxtLink> |
       <NuxtLink :to="localePath('/about')">About</NuxtLink>
     </nav>
-    <form>
-      <select id="locale-select" v-model="locale">
-        <template v-for="code in localeCodes">
-          <option :value="code">{{ code }}</option>
-        </template>
-      </select>
-    </form>
-    <span v-for="locale in availableLocales" :key="locale.code">
-      <NuxtLink :to="switchLocalePath(locale.code) || ''">{{ locale.name }}</NuxtLink> |
-    </span>
+    <h2>Current Language: {{ getLocaleName(locale) }}</h2>
+    <h2>Select Languages</h2>
+    <nav>
+      <span v-for="locale in availableLocales" :key="locale.code">
+        <NuxtLink :to="switchLocalePath(locale.code) || ''">{{ locale.name }}</NuxtLink> |
+      </span>
+    </nav>
+    <h2>I18n Head</h2>
+    <div>
+      <p>{{ i18nHead }}</p>
+    </div>
   </div>
 </template>
