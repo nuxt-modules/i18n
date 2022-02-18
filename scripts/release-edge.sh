2ed172b475c89b727d21af35e21a07c7374dad95
Rafal Chlodnicki <rchl2k@gmail.com>
2022-02-18 11:28:04 +0100

    chore: add scripts for releasing -edge

---
+++ scripts/release-edge.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -xe
+
+# Restore all git changes
+git restore -s@ -SW  -- packages
+
+# Bump versions to edge
+yarn jiti ./scripts/bump-edge
+
+# Resolve yarn
+YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
+
+# Release packages
+# for p in packages/* ; do
+#   pushd $p
+#   echo "Publishing $p"
+#   yarn npm publish --access public --tolerate-republish
+#   popd
+# done
+
