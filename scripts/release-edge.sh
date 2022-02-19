b44fab5a721c9faf4973546b0db21371efb99a28
kazuya kawaguchi <kawakazu80@gmail.com>
2022-02-20 02:53:06 +0900

    fix: use NODE_AUTH_TOKEN on github actions

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -11,6 +11,14 @@ yarn jiti ./scripts/bump-edge
 # Resolve yarn
 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
+# Update token
+if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
+  npm whoami
+fi
+
 # Release packages
 for p in packages/* ; do
   pushd $p
