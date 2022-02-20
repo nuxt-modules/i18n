b408ad84a27b6820b47bb2046ba8c43bf7231431
kazuya kawaguchi <kawakazu80@gmail.com>
2022-02-21 02:44:49 +0900

    fix: updates

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -12,10 +12,11 @@ yarn jiti ./scripts/bump-edge
 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
 # Update token
-if [[ ! -z ${NPM_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
+  echo "npmAuthToken: ${NODE_AUTH_TOKEN}" >> ~/.yarnrc.yml
   npm whoami
 fi
 
