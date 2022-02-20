ef096c38f20b171803066b4debbb682d7bbe8acb
kazuya kawaguchi <kawakazu80@gmail.com>
2022-02-21 01:43:43 +0900

    fix: revert

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -13,9 +13,9 @@ YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
 # Update token
 if [[ ! -z ${NPM_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ./.npmrc
-  echo "registry=https://registry.npmjs.org/" >> ./.npmrc
-  echo "always-auth=true" >> ./.npmrc
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
   npm whoami
 fi
 
