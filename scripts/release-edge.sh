7b0dff953e894320565c19e73d8c706775d7d14f
Rafal Chlodnicki <rchl2k@gmail.com>
2022-02-21 10:17:06 +0100

    .npmrc in hoem

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -13,9 +13,9 @@ YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
 # Update token
 if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ./.npmrc
-  echo "registry=https://registry.npmjs.org/" >> ./.npmrc
-  echo "always-auth=true" >> ./.npmrc
+  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
+  echo "always-auth=true" >> ~/.npmrc
   npm whoami
 fi
 
@@ -23,6 +23,6 @@ fi
 for p in packages/* ; do
   pushd $p
   echo "Publishing $p"
-  npm publish --access public --tolerate-republish
+  yarn npm publish --access public --tolerate-republish
   popd
 done
