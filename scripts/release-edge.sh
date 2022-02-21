7ef165527482a49c404a6087f6c79fd8e0c935e1
Rafal Chlodnicki <rchl2k@gmail.com>
2022-02-21 10:30:03 +0100

    remove debug code

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -12,8 +12,8 @@ yarn jiti ./scripts/bump-edge
 YARN_ENABLE_IMMUTABLE_INSTALLS=false yarn install
 
 # Update token
-if [[ ! -z ${NODE_AUTH_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NODE_AUTH_TOKEN}" >> ~/.npmrc
+if [[ ! -z ${NPM_TOKEN} ]] ; then
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
   echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
   echo "always-auth=true" >> ~/.npmrc
   npm whoami
