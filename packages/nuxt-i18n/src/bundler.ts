4e4cffe6578b6244444ac43e7da1c3606ea68d76
kazuya kawaguchi <kawakazu80@gmail.com>
2022-05-07 15:26:24 +0900

    integrate unplugin-vue-i18n

--- packages/nuxt-i18n/src/bundler.ts
+++ packages/nuxt-i18n/src/bundler.ts
@@ -1,9 +1,8 @@
 import createDebug from 'debug'
 import { resolve } from 'pathe'
-import { vueI18n } from '@intlify/vite-plugin-vue-i18n'
-import { extendViteConfig, extendWebpackConfig } from '@nuxt/kit'
-
-type VitePluginOptions = Parameters<typeof vueI18n>[0]
+import { extendWebpackConfig, addWebpackPlugin, addVitePlugin } from '@nuxt/kit'
+import VueI18nWebpackPlugin from '@intlify/unplugin-vue-i18n/webpack'
+import VueI18nVitePlugin from '@intlify/unplugin-vue-i18n/vite'
 
 const debug = createDebug('@nuxtjs/i18n:bundler')
 
@@ -12,23 +11,16 @@ export async function extendBundler(hasLocaleFiles: boolean, langPath: string | 
     // @ts-ignore NOTE: use webpack which is installed by nuxt
     const webpack = await import('webpack').then(m => m.default || m)
 
-    // TODO: vue-i18n-loader cannot be resolved as i18n resources which compiled by the intlify message compiler ...
-    extendWebpackConfig(config => {
-      if (hasLocaleFiles && langPath) {
-        config.module!.rules!.push({
-          test: /\.(json5?|ya?ml)$/,
-          type: 'javascript/auto',
-          loader: '@intlify/vue-i18n-loader',
+    // install webpack plugin
+    if (hasLocaleFiles && langPath) {
+      addWebpackPlugin(
+        VueI18nWebpackPlugin({
           include: [resolve(langPath, './**')]
         })
-      }
+      )
+    }
 
-      config.module!.rules!.push({
-        resourceQuery: /blockType=i18n/,
-        type: 'javascript/auto',
-        loader: '@intlify/vue-i18n-loader'
-      })
-
+    extendWebpackConfig(config => {
       // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- `config.plugins` is safe, so it's assigned with nuxt!
       config.plugins!.push(
         new webpack.DefinePlugin({
@@ -42,15 +34,12 @@ export async function extendBundler(hasLocaleFiles: boolean, langPath: string | 
     debug((e as Error).message)
   }
 
-  // install @intlify/vite-plugin-vue-i18n
-  extendViteConfig(config => {
-    const viteOptions: VitePluginOptions = {
-      compositionOnly: false
-    }
-    if (hasLocaleFiles && langPath) {
-      viteOptions['include'] = resolve(langPath, './**')
-    }
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion -- `config.plugins` is safe, so it's assigned with nuxt!
-    config.plugins!.push(vueI18n(viteOptions))
-  })
+  // install vite plugin
+  if (hasLocaleFiles && langPath) {
+    addVitePlugin(
+      VueI18nVitePlugin({
+        include: [resolve(langPath, './**')]
+      })
+    )
+  }
 }
