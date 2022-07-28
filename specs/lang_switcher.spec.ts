6a4d292df97ffaa81bc98715a29f213076f99499
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-29 00:07:45 +0900

    add lang swicher specs

---
+++ specs/lang_switcher.spec.ts
@@ -0,0 +1,67 @@
+import { test, expect } from 'vitest'
+import { fileURLToPath } from 'node:url'
+import { setup, url, createPage } from '@nuxt/test-utils'
+import { getText, getData } from './helper'
+
+await setup({
+  rootDir: fileURLToPath(new URL(`./fixtures/basic`, import.meta.url)),
+  browser: true,
+  // overrides
+  nuxtConfig: {
+    i18n: {
+      defaultLocale: 'en'
+    }
+  }
+})
+
+test('<NuxtLink>', async () => {
+  const home = url('/')
+  const page = await createPage()
+  await page.goto(home)
+
+  // click `fr` lang switch with `<NuxtLink>`
+  await page.locator('#lang-switcher-with-nuxt-link a').click()
+
+  // `fr` rendering
+  expect(await getText(page, '#home-header')).toMatch('Accueil')
+  expect(await getText(page, '#link-about')).toMatch('À propos')
+
+  // lang switcher rendering
+  expect(await getText(page, '#lang-switcher-with-nuxt-link a')).toMatch('English')
+  expect(await getText(page, '#set-locale-link-en')).toMatch('English')
+
+  // page path
+  expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/fr/about' })
+  expect(await page.getAttribute('#lang-switcher-with-nuxt-link a', 'href')).toMatch('/')
+
+  // current locale
+  expect(await getText(page, '#lang-switcher-current-locale')).toMatch('fr')
+})
+
+test('setLocale', async () => {
+  const home = url('/fr')
+  const page = await createPage()
+  await page.goto(home)
+
+  // click `en` lang switch with `setLocale`
+  await page.locator('#lang-switcher-with-set-locale a').click()
+
+  // `en` rendering
+  expect(await getText(page, '#home-header')).toMatch('Homepage')
+  expect(await getText(page, '#link-about')).toMatch('About us')
+
+  // lang switcher rendering
+  expect(await getText(page, '#lang-switcher-with-nuxt-link a')).toMatch('Français')
+  expect(await getText(page, '#set-locale-link-fr')).toMatch('Français')
+
+  // page path
+  expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/about' })
+  expect(await page.getAttribute('#lang-switcher-with-nuxt-link a', 'href')).toMatch('/fr')
+
+  // current locale
+  expect(await getText(page, '#lang-switcher-current-locale')).toMatch('en')
+})
+
+test.todo('dynamic route parameter')
+
+test.todo('wait for page transition')
--- specs/lazy_load.spec.ts
+++ specs/lazy_load.spec.ts
@@ -1,83 +1,81 @@
-import { describe, test, expect } from 'vitest'
+import { test, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
 import { setup, url, createPage } from '@nuxt/test-utils'
 import { getText, getData } from './helper'
 
-describe('lazy load', async () => {
-  await setup({
-    rootDir: fileURLToPath(new URL(`./fixtures/lazy`, import.meta.url)),
-    browser: true,
-    // overrides
-    nuxtConfig: {
-      i18n: {
-        defaultLocale: 'en',
-        langDir: 'lang',
-        lazy: true,
-        locales: [
-          {
-            code: 'en',
-            iso: 'en-US',
-            file: 'en.json',
-            name: 'English'
-          },
-          {
-            code: 'fr',
-            iso: 'fr-FR',
-            file: 'fr.json5',
-            name: 'Français'
-          }
-        ]
-      }
+await setup({
+  rootDir: fileURLToPath(new URL(`./fixtures/lazy`, import.meta.url)),
+  browser: true,
+  // overrides
+  nuxtConfig: {
+    i18n: {
+      defaultLocale: 'en',
+      langDir: 'lang',
+      lazy: true,
+      locales: [
+        {
+          code: 'en',
+          iso: 'en-US',
+          file: 'en.json',
+          name: 'English'
+        },
+        {
+          code: 'fr',
+          iso: 'fr-FR',
+          file: 'fr.json5',
+          name: 'Français'
+        }
+      ]
     }
-  })
+  }
+})
 
-  test('can access to no prefix locale (en): /', async () => {
-    const home = url('/')
-    const page = await createPage()
-    await page.goto(home)
+test('can access to no prefix locale (en): /', async () => {
+  const home = url('/')
+  const page = await createPage()
+  await page.goto(home)
 
-    // `en` rendering
-    expect(await getText(page, '#home-header')).toMatch('Homepage')
-    expect(await getText(page, 'title')).toMatch('Homepage')
-    expect(await getText(page, '#link-about')).toMatch('About us')
+  // `en` rendering
+  expect(await getText(page, '#home-header')).toMatch('Homepage')
+  expect(await getText(page, 'title')).toMatch('Homepage')
+  expect(await getText(page, '#link-about')).toMatch('About us')
 
-    // lang switcher rendering
-    expect(await getText(page, '#lang-switcher-with-nuxt-link a')).toMatch('Français')
-    expect(await getText(page, '#set-locale-link-fr')).toMatch('Français')
+  // lang switcher rendering
+  expect(await getText(page, '#lang-switcher-with-nuxt-link a')).toMatch('Français')
+  expect(await getText(page, '#set-locale-link-fr')).toMatch('Français')
 
-    // page path
-    expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/about' })
-    expect(await page.getAttribute('#lang-switcher-with-nuxt-link a', 'href')).toMatch('/fr')
+  // page path
+  expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/about' })
+  expect(await page.getAttribute('#lang-switcher-with-nuxt-link a', 'href')).toMatch('/fr')
 
-    // current locale
-    expect(await getText(page, '#lang-switcher-current-locale')).toMatch('en')
+  // current locale
+  expect(await getText(page, '#lang-switcher-current-locale')).toMatch('en')
 
-    // html tag `lang` attriute with iso code
-    expect(await page.getAttribute('html', 'lang')).toMatch('en-US')
-  })
+  // html tag `lang` attriute with iso code
+  expect(await page.getAttribute('html', 'lang')).toMatch('en-US')
+})
 
-  test('can access to prefix locale: /fr', async () => {
-    const home = url('/fr')
-    const page = await createPage()
-    await page.goto(home)
+test('can access to prefix locale: /fr', async () => {
+  const home = url('/fr')
+  const page = await createPage()
+  await page.goto(home)
 
-    // `fr` rendering
-    expect(await getText(page, '#home-header')).toMatch('Accueil')
-    expect(await getText(page, 'title')).toMatch('Accueil')
-    expect(await getText(page, '#link-about')).toMatch('À propos')
+  // `fr` rendering
+  expect(await getText(page, '#home-header')).toMatch('Accueil')
+  expect(await getText(page, 'title')).toMatch('Accueil')
+  expect(await getText(page, '#link-about')).toMatch('À propos')
 
-    // lang switcher rendering
-    expect(await getText(page, '#lang-switcher-with-nuxt-link a')).toMatch('English')
-    expect(await getText(page, '#set-locale-link-en')).toMatch('English')
+  // lang switcher rendering
+  expect(await getText(page, '#lang-switcher-with-nuxt-link a')).toMatch('English')
+  expect(await getText(page, '#set-locale-link-en')).toMatch('English')
 
-    // page path
-    expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/fr/about' })
-    expect(await page.getAttribute('#lang-switcher-with-nuxt-link a', 'href')).toMatch('/')
+  // page path
+  expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/fr/about' })
+  expect(await page.getAttribute('#lang-switcher-with-nuxt-link a', 'href')).toMatch('/')
 
-    // current locale
-    expect(await getText(page, '#lang-switcher-current-locale')).toMatch('fr')
+  // current locale
+  expect(await getText(page, '#lang-switcher-current-locale')).toMatch('fr')
 
-    // html tag `lang` attriute with iso code
-    expect(await page.getAttribute('html', 'lang')).toMatch('fr-FR')
-  })
+  // html tag `lang` attriute with iso code
+  expect(await page.getAttribute('html', 'lang')).toMatch('fr-FR')
 })
