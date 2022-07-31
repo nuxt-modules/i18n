50ec1c02c74fdfa14ae2fb90c47b231269ea2550
kazuya kawaguchi <kawakazu80@gmail.com>
2022-07-31 20:45:29 +0900

    add fallback spec

---
+++ specs/fallback.spec.ts
@@ -0,0 +1,38 @@
+import { test, expect } from 'vitest'
+import { fileURLToPath } from 'node:url'
+import { setup, url, createPage } from '@nuxt/test-utils'
+import { getText, getData } from './helper'
+
+await setup({
+  rootDir: fileURLToPath(new URL(`./fixtures/fallback`, import.meta.url)),
+  browser: true,
+  // overrides
+  nuxtConfig: {
+    i18n: {}
+  }
+})
+
+test('fallback to target lang', async () => {
+  const home = url('/')
+  const page = await createPage()
+  await page.goto(home)
+
+  // `en` rendering
+  expect(await getText(page, '#home-header')).toEqual('Homepage')
+  expect(await getText(page, 'title')).toEqual('Homepage')
+  expect(await getText(page, '#link-about')).toEqual('About us')
+
+  // click `ja` lang switch with `<NuxtLink>`
+  await page.locator('#lang-switcher-with-nuxt-link-ja a').click()
+
+  // fallback to en content translation
+  expect(await getText(page, '#home-header')).toEqual('Homepage')
+  expect(await getText(page, 'title')).toEqual('Homepage')
+  expect(await getText(page, '#link-about')).toEqual('About us')
+
+  // page path
+  expect(await getData(page, '#home-use-async-data')).toMatchObject({ aboutPath: '/ja/about' })
+
+  // current locale
+  expect(await getText(page, '#lang-switcher-current-locale code')).toEqual('ja')
+})
