cc435e6fdffe9c1ec5602b081f1af98601eb0f20
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-13 05:12:10 +0900

    add basic usage spec test

---
+++ specs/fixtures/basic_usage/nuxt.config.ts
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from 'nuxt'
+import I18nModule from '../../..'
+
+// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  modules: [I18nModule],
+
+  i18n: {
+    vueI18n: {
+      legacy: false,
+      locale: 'en',
+      messages: {
+        fr: {
+          welcome: 'Bienvenue',
+          home: 'Accueil',
+          profile: 'Profil'
+        },
+        en: {
+          welcome: 'Welcome',
+          home: 'Homepage',
+          profile: 'Profile'
+        }
+      }
+    }
+  }
+})
