ab46380425aec1b974fb66c4ab0819b5b70c37b3
kazuya kawaguchi <kawakazu80@gmail.com>
2022-08-21 03:52:04 +0900

    updates

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,10 +1,11 @@
 import { defineNuxtConfig } from 'nuxt'
+import overrideModule from './module'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  extends: ['../node_modules/@docus/docs-theme'],
+  extends: ['@docus/docs-theme'],
 
-  modules: ['@docus/github'],
+  modules: ['@nuxthq/admin', '@docus/github', 'vue-plausible', overrideModule],
 
   github: {
     owner: 'nuxt-community',
@@ -12,43 +13,7 @@ export default defineNuxtConfig({
     branch: 'next'
   },
 
-  tailwindcss: {
-    // @ts-ignore
-    config: {
-      theme: {
-        extend: {
-          colors: {
-            primary: {
-              50: '#d6ffee',
-              100: '#acffdd',
-              200: '#83ffcc',
-              300: '#30ffaa',
-              400: '#00dc82',
-              500: '#00bd6f',
-              600: '#009d5d',
-              700: '#007e4a',
-              800: '#005e38',
-              900: '#003f25'
-            }
-            /*
-            primary: {
-              // DEFUALT: '#41B38A'
-              DEFAULT: '#5468FF',
-              50: '#d6ffee',
-              100: '#acffdd',
-              200: '#83ffcc',
-              300: '#30ffaa',
-              400: '#00dc82',
-              500: '#00bd6f',
-              600: '#009d5d',
-              700: '#007e4a',
-              800: '#005e38',
-              900: '#003f25'
-            }
-            */
-          }
-        }
-      }
-    }
+  plausible: {
+    domain: 'i18n.nuxtjs.org'
   }
 })
