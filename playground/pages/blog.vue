697d7ef2d9c7546f59999a78a8f283228a210714
kazuya kawaguchi <kawakazu80@gmail.com>
2022-10-18 18:19:00 +0900

    feat: support per-component translations (#1567)
    * feat: support per-component translations
    
    * update

--- playground/pages/blog.vue
+++ playground/pages/blog.vue
@@ -1,10 +1,6 @@
-<template>
-  <div>
-    <h1>This is blog page</h1>
-  </div>
-</template>
+<script setup lang="ts">
+import { useI18n } from '#i18n'
 
-<script setup>
 defineI18nRoute({
   paths: {
     en: '/blog-us',
@@ -12,4 +8,28 @@ defineI18nRoute({
     ja: '/blog-ja'
   }
 })
+
+const { t } = useI18n({
+  useScope: 'local'
+})
 </script>
+
+<template>
+  <div>
+    <h1>{{ t('desc') }}</h1>
+  </div>
+</template>
+
+<i18n lang="json">
+{
+  "en": {
+    "desc": "This is blog page"
+  },
+  "fr": {
+    "desc": "Ceci est la page blog"
+  },
+  "ja": {
+    "desc": "これはブログページです"
+  }
+}
+</i18n>
