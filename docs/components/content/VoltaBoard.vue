00847ad87c54d0496f0b6b7a1a33dc0cac558f7a
kazuya kawaguchi <kawakazu80@gmail.com>
2022-11-09 13:35:33 +0900

    docs: add roadmap

---
+++ docs/components/content/VoltaBoard.vue
@@ -0,0 +1,13 @@
+<template>
+  <iframe :src="src" class="w-full min-h-[calc(100vh/1.5)] border u-border-gray-200 shadow-lg rounded-md" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  token: {
+    type: String,
+    required: true
+  }
+})
+const src = computed(() => `https://volta.net/embed/${props.token}`)
+</script>
