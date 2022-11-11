bc06d8993c4c525ed1f6aa914887729858c4f639
kazuya kawaguchi <kawakazu80@gmail.com>
2022-11-12 00:37:59 +0900

    fix: extract defineI18nRoute options fully (#1636)

---
+++ test/fixtures/custom_route/js/pages/about.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { defineI18nRoute } from '#i18n'
+
+defineI18nRoute({
+  paths: {
+    en: '/about-us',
+    fr: '/a-propos',
+    ja: '/about-ja'
+  }
+})
+</script>
+
+<template>
+  <p>Foo</p>
+</template>
+
+<style>
+p {
+  color: red;
+}
+</style>
---
+++ test/fixtures/custom_route/with_meta/pages/about.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { defineI18nRoute } from '#i18n'
+
+defineI18nRoute({
+  paths: {
+    en: '/about-us',
+    fr: '/a-propos',
+    ja: '/about-ja'
+  }
+})
+
+definePageMeta({
+  layout: false
+})
+</script>
