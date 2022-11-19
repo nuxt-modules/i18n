d299d747dec7c70d57daa67c70684fc5a97ede85
kazuya kawaguchi <kawakazu80@gmail.com>
2022-11-19 15:23:06 +0900

    fix: v8 docs with official release version
    resolve #1631
    resolve #1656

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,30 +1,3 @@
-import overrideModule from './module'
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  extends: ['@docus/docs-theme'],
-
-  modules: ['@nuxthq/admin', '@docus/github', 'vue-plausible', overrideModule],
-
-  // content: {
-  //   sources: [
-  //     {
-  //       name: 'next',
-  //       prefix: '/next',
-  //       driver: 'fs',
-  //       // base: resolve(__dirname, './content-next')
-  //       base: fileURLToPath(new URL('./content-next', import.meta.url))
-  //     }
-  //   ]
-  // },
-
-  github: {
-    owner: 'nuxt-community',
-    repo: 'i18n-module',
-    branch: 'next'
-  },
-
-  plausible: {
-    domain: 'i18n.nuxtjs.org'
-  }
+  extends: ['@nuxt-themes/docus']
 })
