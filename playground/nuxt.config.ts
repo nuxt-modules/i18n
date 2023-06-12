3d2d92f399ba3728998e98c6f50f0b8f417f64a4
kazuya kawaguchi <kawakazu80@gmail.com>
2023-06-12 19:34:27 +0900

    fix: resolve i18n configuration for array module options

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -11,6 +11,81 @@ export default defineNuxtConfig({
     },
     Module1,
     LayerModule,
+    /*
+    [
+      '@nuxtjs/i18n',
+      {
+        experimental: {
+          jsTsFormatResource: true
+        },
+        precompile: {
+          strictMessage: false,
+          escapeHtml: true
+        },
+        langDir: 'locales',
+        lazy: true,
+        baseUrl: 'http://localhost:3000',
+        locales: [
+          {
+            code: 'en',
+            iso: 'en-US',
+            file: 'en.json',
+            // domain: 'localhost',
+            name: 'English'
+          },
+          {
+            code: 'en-GB',
+            iso: 'en-GB',
+            files: ['en.json', 'en-GB.js', 'en-KK.js'],
+            name: 'English (UK)'
+          },
+          {
+            code: 'ja',
+            iso: 'ja-JP',
+            file: 'ja.ts',
+            domain: 'mydomain.com',
+            name: 'Japanses'
+          },
+          {
+            code: 'fr',
+            iso: 'fr-FR',
+            file: 'fr.json',
+            domain: 'mydomain.fr',
+            name: 'Français'
+          }
+        ],
+        // trailingSlash: true,
+        debug: false,
+        defaultLocale: 'en',
+        // strategy: 'no_prefix',
+        // strategy: 'prefix',
+        // strategy: 'prefix_and_default',
+        strategy: 'prefix_except_default',
+        // rootRedirect: '/ja/about-ja',
+        dynamicRouteParams: true,
+        // customRoutes: 'config',
+        pages: {
+          history: {
+            ja: '/history-ja'
+          },
+          about: {
+            ja: '/about-ja'
+          }
+        },
+        // differentDomains: true,
+        // skipSettingLocaleOnNavigate: true,
+        // detectBrowserLanguage: false,
+        detectBrowserLanguage: {
+          useCookie: true
+          // alwaysRedirect: true
+          // cookieKey: 'i18n_redirected',
+          // // cookieKey: 'my_custom_cookie_name',
+          // redirectOn: 'root'
+        },
+        vueI18n: './vue-i18n.options.ts'
+      }
+    ],
+  */
     '@nuxtjs/i18n',
     '@nuxt/devtools'
   ],
