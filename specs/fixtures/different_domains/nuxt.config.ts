cd89a4e9402340405ea0359b3198096eec0a7ab6
Bobbie Goede <bobbiegoede@gmail.com>
2023-06-21 14:45:42 +0200

    test: add layer domain merge tests

---
+++ specs/fixtures/different_domains/nuxt.config.ts
@@ -0,0 +1,39 @@
+import CustomModule from './module'
+
+// https://nuxt.com/docs/guide/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  modules: [CustomModule, '@nuxtjs/i18n'],
+
+  i18n: {
+    lazy: false,
+    baseUrl: 'http://localhost:3000',
+    locales: [
+      {
+        code: 'ja',
+        iso: 'ja',
+        name: 'Japanese'
+      },
+      {
+        code: 'en',
+        iso: 'en',
+        name: 'English',
+        domain: 'project-en.example.com'
+      },
+      {
+        code: 'fr',
+        iso: 'fr-FR',
+        name: 'Français'
+        // domain: 'project-fr.example.com'
+      },
+      {
+        code: 'nl',
+        iso: 'nl-NL',
+        file: 'nl.json',
+        domain: undefined,
+        name: 'Nederlands'
+      }
+    ],
+    defaultLocale: 'ja',
+    detectBrowserLanguage: false
+  }
+})
