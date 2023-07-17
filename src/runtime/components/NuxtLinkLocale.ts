b057a442c2fa3fa1adbc70662a8bcc1489d837e0
Bobbie Goede <bobbiegoede@gmail.com>
2023-07-17 11:13:00 +0200

    fix: hard-coded NuxtLinkLocale props

--- src/runtime/components/NuxtLinkLocale.ts
+++ src/runtime/components/NuxtLinkLocale.ts
@@ -2,7 +2,6 @@ import { useLocalePath } from '#i18n'
 import { defineComponent, computed, defineNuxtLink, h } from '#imports'
 
 import type { PropType } from 'vue'
-import type { RouteLocationRaw } from 'vue-router'
 import type { RawLocation, RouteLocation } from '@intlify/vue-router-bridge'
 
 const NuxtLinkLocale = defineNuxtLink({ componentName: 'NuxtLinkLocale' })
@@ -10,6 +9,7 @@ const NuxtLinkLocale = defineNuxtLink({ componentName: 'NuxtLinkLocale' })
 export default defineComponent({
   name: 'NuxtLinkLocale',
   props: {
+    ...NuxtLinkLocale.props,
     to: {
       type: [String, Object] as PropType<RawLocation | RouteLocation>,
       default: undefined,
@@ -17,85 +17,6 @@ export default defineComponent({
     },
     locale: {
       type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-
-    // Default NuxtLink props
-    href: {
-      type: [String, Object] as PropType<RouteLocationRaw>,
-      default: undefined,
-      required: false
-    },
-
-    // Attributes
-    target: {
-      type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-    rel: {
-      type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-    noRel: {
-      type: Boolean as PropType<boolean>,
-      default: undefined,
-      required: false
-    },
-
-    // Prefetching
-    prefetch: {
-      type: Boolean as PropType<boolean>,
-      default: undefined,
-      required: false
-    },
-    noPrefetch: {
-      type: Boolean as PropType<boolean>,
-      default: undefined,
-      required: false
-    },
-
-    // Styling
-    activeClass: {
-      type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-    exactActiveClass: {
-      type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-    prefetchedClass: {
-      type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-
-    // Vue Router's `<RouterLink>` additional props
-    replace: {
-      type: Boolean as PropType<boolean>,
-      default: undefined,
-      required: false
-    },
-    ariaCurrentValue: {
-      type: String as PropType<string>,
-      default: undefined,
-      required: false
-    },
-
-    // Edge cases handling
-    external: {
-      type: Boolean as PropType<boolean>,
-      default: undefined,
-      required: false
-    },
-
-    // Slot API
-    custom: {
-      type: Boolean as PropType<boolean>,
       default: undefined,
       required: false
     }
@@ -107,4 +28,3 @@ export default defineComponent({
     return () => h(NuxtLinkLocale, { ...props, to: resolvedPath }, slots.default)
   }
 })
-
