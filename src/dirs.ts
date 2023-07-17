eb55bc398ac3896bb1a01e4ca6e0e723305552e7
kazuya kawaguchi <kawakazu80@gmail.com>
2023-07-18 00:37:34 +0900

    fix: deps resolving for alias (#2239)

--- src/dirs.ts
+++ src/dirs.ts
@@ -1,8 +1,6 @@
 import createDebug from 'debug'
 import { fileURLToPath } from 'node:url'
-import { dirname, resolve, parse as parsePath } from 'pathe'
-import { VUE_I18N_ROUTING_PKG } from './constants'
-import { getPackageManagerType, tryResolve } from './utils'
+import { dirname, resolve } from 'pathe'
 
 const debug = createDebug('@nuxtjs/i18n:dirs')
 
@@ -15,19 +13,3 @@ debug('distDir', distDir)
 debug('runtimeDir', runtimeDir)
 debug('pkgDir', pkgDir)
 debug('pkgModulesDir', pkgModulesDir)
-
-export async function resolveVueI18nRoutingDtsPath(id: string, rootDir: string) {
-  const pkgMgr = await getPackageManagerType()
-  const dtsPath = `${VUE_I18N_ROUTING_PKG}/dist/${id}`
-  const targets = [
-    // 1st, try to resolve from `node_modules` (hoisted case)
-    resolve(rootDir, 'node_modules', dtsPath),
-    // 2nd, try to resolve from `node_modules/@nuxtjs/i18n` (not hoisted case)
-    resolve(pkgModulesDir, dtsPath)
-  ]
-  debug(`${VUE_I18N_ROUTING_PKG} resolving from ...`, targets)
-
-  const resolved = await tryResolve(VUE_I18N_ROUTING_PKG, targets, pkgMgr, '.d.ts')
-  const parsed = parsePath(resolved)
-  return `${parsed.dir}/${parsed.name}`
-}
