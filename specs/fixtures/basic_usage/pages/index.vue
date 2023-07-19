9dfb024a6e31cf257fd004d1e375226bd4db287f
Bobbie Goede <bobbiegoede@gmail.com>
2023-07-19 17:25:55 +0200

    feat: add NuxtLinkLocale component (#2229)
    * feat: add NuxtLinkLocale component
    
    * refactor: rewrite NuxtLinkLocale sfc as render function
    
    * fix: path resolution for #imports alias
    
    * docs: add components page
    
    * test: add NuxtLinkLocale tests
    
    * fix: hard-coded NuxtLinkLocale props
    
    * docs: change component example to be more realistic
    
    * fix: move nuxi prepare from postinstall to test script
    
    * fix: add dev:prepare script to run before dev and test scripts

--- specs/fixtures/basic_usage/pages/index.vue
+++ specs/fixtures/basic_usage/pages/index.vue
@@ -40,7 +40,7 @@ function onClick() {
           <NuxtLink :to="localePath('/')">{{ $t('home') }}</NuxtLink>
         </li>
         <li class="named-with-locale">
-          <NuxtLink :to="localePath('index', 'en')">Homepage in English</NuxtLink>
+          <NuxtLink :to="localePath('index', 'fr')">Homepage in French</NuxtLink>
         </li>
         <li class="nest-path">
           <NuxtLink :to="localePath('/user/profile')">Route by path to: {{ $t('profile') }}</NuxtLink>
@@ -52,6 +52,31 @@ function onClick() {
           <NuxtLink :to="localePath({ name: 'category-slug', params: { slug: category.slug } })">
             {{ category.title }}
           </NuxtLink>
+        </li>
+      </ul>
+    </section>
+	    <section id="nuxt-link-locale-usages">
+      <h3>NuxtLinkLocale</h3>
+      <ul>
+        <li class="name">
+          <NuxtLinkLocale :to="'index'">{{ $t('home') }}</NuxtLinkLocale>
+        </li>
+        <li class="path">
+          <NuxtLinkLocale :to="'/'">{{ $t('home') }}</NuxtLinkLocale>
+        </li>
+        <li class="named-with-locale">
+          <NuxtLinkLocale :to="'index'" :locale="'fr'">Homepage in French</NuxtLinkLocale>
+        </li>
+        <li class="nest-path">
+          <NuxtLinkLocale :to="'/user/profile'">Route by path to: {{ $t('profile') }}</NuxtLinkLocale>
+        </li>
+        <li class="nest-named">
+          <NuxtLinkLocale :to="'user-profile'">Route by name to: {{ $t('profile') }}</NuxtLinkLocale>
+        </li>
+        <li class="object-with-named">
+          <NuxtLinkLocale :to="{ name: 'category-slug', params: { slug: category.slug } }">
+            {{ category.title }}
+          </NuxtLinkLocale>
         </li>
       </ul>
     </section>
