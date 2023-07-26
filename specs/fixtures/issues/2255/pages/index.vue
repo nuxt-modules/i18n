55f2d11947d135aec3bbad67cb9dafe2595ce704
kazuya kawaguchi <kawakazu80@gmail.com>
2023-07-26 14:35:32 +0900

    fix: does not redirect when the site is reload again (#2263)

---
+++ specs/fixtures/issues/2255/pages/index.vue
@@ -0,0 +1,21 @@
+<script setup>
+const { locale, locales } = useI18n()
+const switchLocalePath = useSwitchLocalePath()
+const availableLocales = computed(() => {
+  return locales.value.filter(i => i.code !== locale.value)
+})
+</script>
+
+<template>
+  <div>
+    <NuxtLink
+      :id="locale.code"
+      v-for="locale in availableLocales"
+      :key="locale.code"
+      :to="switchLocalePath(locale.code)"
+      >{{ locale.name }}</NuxtLink
+    >
+  </div>
+
+  <p id="msg">{{ $t('welcome') }}</p>
+</template>
