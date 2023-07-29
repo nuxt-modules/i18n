42028bc0e67012371aa72195fbb226f87a869259
Titus Kirch <titus.kirch@kirch.dev>
2023-07-29 22:12:38 +0200

    Add is external logic from NuxtLink component

--- src/runtime/components/NuxtLinkLocale.ts
+++ src/runtime/components/NuxtLinkLocale.ts
@@ -1,5 +1,7 @@
+import { NuxtLink } from '#components';
 import { useLocalePath } from '#i18n'
 import { defineComponent, computed, defineNuxtLink, h } from '#imports'
+import { hasProtocol } from 'ufo';
 
 import type { PropType } from 'vue'
 import type { RawLocation, RouteLocation } from '@intlify/vue-router-bridge'
@@ -23,8 +25,32 @@ export default defineComponent({
   },
   setup(props, { slots }) {
     const localePath = useLocalePath()
-    const resolvedPath = computed(() => (props.to != null ? localePath(props.to, props.locale) : props.to))
+    const resolvedPath = computed(() =>
+      props.to != null ? localePath(props.to, props.locale) : props.to
+    );
 
-    return () => h(NuxtLinkLocale, { ...props, to: resolvedPath }, slots.default)
+    // Resolving link type
+    const isExternal = computed<boolean>(() => {
+      // External prop is explicitly set
+      if (props.external) {
+        return true;
+      }
+
+      // When `target` prop is set, link is external
+      if (props.target && props.target !== '_self') {
+        return true;
+      }
+
+      // When `to` is a route object then it's an internal link
+      if (typeof props.to === 'object') {
+        return false;
+      }
+
+      return props.to === '' || hasProtocol(props.to, { acceptRelative: true });
+    });
+
+    return () => isExternal.value
+        ? h(NuxtLink, props, slots.default)
+        : h(NuxtLinkLocale, { ...props, to: resolvedPath }, slots.default);
   }
 })
