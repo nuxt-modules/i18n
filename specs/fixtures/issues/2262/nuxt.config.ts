23bf452a17e2e0fa6e49a1ce5f036cb8a6a34d65
kazuya kawaguchi <kawakazu80@gmail.com>
2023-07-31 00:21:48 +0900

    fix: `detectBrowserLanguage` option
    resolve #2262
    ref: #1888

---
+++ specs/fixtures/issues/2262/nuxt.config.ts
@@ -0,0 +1,25 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        name: 'English'
+      },
+      {
+        code: 'fr',
+        name: 'Français'
+      }
+    ],
+    strategy: 'prefix_except_default',
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root', // recommended
+      alwaysRedirect: true
+    }
+  }
+})
