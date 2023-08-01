9904cf118fd11398ab6938d7d1adfc16c3c08167
Bobbie Goede <bobbiegoede@gmail.com>
2023-08-01 18:25:09 +0200

    test: layers vueI18n options test

---
+++ specs/fixtures/layer_consumer/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+section {
+  margin: 1rem 0;
+}
+</style>
---
+++ specs/fixtures/layer_consumer/nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/guide/directory-structure/nuxt.config
+export default defineNuxtConfig({
+  extends: ['./layer-simple'],
+  modules: ['@nuxtjs/i18n']
+})
