7930e1734ec12934f6d7006ea20d58e67f0f2691
kazuya kawaguchi <kawakazu80@gmail.com>
2023-08-17 16:05:56 +0900

    docs: fix roadmap layout

--- docs/components/content/VoltaBoard.vue
+++ docs/components/content/VoltaBoard.vue
@@ -1,5 +1,5 @@
 <template>
-  <iframe :src="src" class="w-full min-h-[calc(100vh/1.5)] border u-border-gray-200 shadow-lg rounded-md" />
+  <iframe :src="src" style="width: 100%; min-height: calc(100vh / 1.5)" />
 </template>
 
 <script setup lang="ts">
