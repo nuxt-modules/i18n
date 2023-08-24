31406183571e72196dea17a062cee20abd5906ff
kazuya kawaguchi <kawakazu80@gmail.com>
2023-08-24 18:16:19 +0900

    fix: regression for API extending

---
+++ specs/fixtures/issues/2338/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <Hello />
+    <code id="global">{{ localeProperties }}</code>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { localeProperties } = useI18n()
+</script>
