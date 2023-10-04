7021bcb93acd57e5b88938d738a74ac38d720259
kazuya kawaguchi <kawakazu80@gmail.com>
2023-10-04 23:53:22 +0900

    fix: not work detectBrowserLanguage on client-side correctly (#2475)
    * fix: not work detectBrowserLanguage on client-side correctly
    
    * fix

---
+++ specs/fixtures/issues/2473/pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+  <h1 id="title" v-text="$t('Pages.Home.title')" />
+  <p v-text="$t('Pages.Home.description')" />
+  <p v-text="$t('Meta.locale')" />
+  <NuxtLinkLocale id="locale" to="/about">{{ $t('Pages.About.title') }}</NuxtLinkLocale>
+</template>
