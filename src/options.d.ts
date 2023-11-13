cc3637ecedf747a6ea3e6a77c1886e64514d0931
Bobbie Goede <bobbiegoede@gmail.com>
2023-11-13 11:28:15 +0100

    refactor: resolve vue i18n options

--- src/options.d.ts
+++ src/options.d.ts
@@ -2,7 +2,7 @@
 import type { NuxtI18nOptions, NuxtI18nInternalOptions, RootRedirectOptions } from './types'
 import type { NuxtI18nOptionsDefault } from './constants'
 import type { DeepRequired } from 'ts-essentials'
-import type { NuxtApp } from '@nuxt/schema'
+import type { I18nOptions } from 'vue-i18n'
 
 /**
  * stub type definition for @nuxtjs/i18n internally
@@ -19,16 +19,12 @@ type LocaleLoader = {
 export const loadMessages: () => Promise<any> = () => Promise.resolve({})
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const localeMessages: Record<string, LocaleLoader[]> = {}
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const resolveNuxtI18nOptions: <Context = NuxtApp>(
-  context: Context
-) => Promise<DeepRequired<NuxtI18nOptions<Context>>> = () => Promise.resolve({})
-export type LoaderResult = Record<string, unknown> | (() => Record<string, unknown>)
-type LoadedConfig = Record<string, unknown>
-export type VLoaderType = () => Promise<{ default: LoadedConfig | (() => LoadedConfig) }>
-export const vueI18nConfigLoaders: VLoaderType[]
+
+export type VueI18nConfig = () => Promise<{ default: I18nOptions | (() => I18nOptions | Promise<I18nOptions>) }>
+export const vueI18nConfigs: VueI18nConfig[]
+
 export const localeCodes: string[] = []
-export const nuxtI18nOptions: DeepRequired<NuxtI18nOptions> = {}
+export const nuxtI18nOptions: DeepRequired<NuxtI18nOptions<Context>> = {}
 export const nuxtI18nOptionsDefault: NuxtI18nOptionsDefault = {}
 export const nuxtI18nInternalOptions: DeepRequired<NuxtI18nInternalOptions> = {}
 export const NUXT_I18N_MODULE_ID = ''
