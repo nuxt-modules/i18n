8bfafd510a69fdc66c0a2b632a27a1270c779841
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-11-16 00:40:39 +0100

    test(site-url): use hook for url configuration

--- specs/seo/siteUrl.spec.ts
+++ specs/seo/siteUrl.spec.ts
@@ -3,27 +3,31 @@ import { fileURLToPath } from 'node:url'
 import { setup, $fetch } from '../utils'
 import { getDom, assertLocaleHeadWithDom } from '../helper'
 
+const getUrl = () => {
+  if (process.server) {
+    // @ts-ignore
+    const headers = useRequestHeaders(['x-override-base-url'])
+    const xOverrideBaseUrl = headers['x-override-base-url']
+    console.log('xOverrideBaseUrl', xOverrideBaseUrl)
+    if (Array.isArray(xOverrideBaseUrl)) {
+      return xOverrideBaseUrl[0]
+    }
+    return xOverrideBaseUrl || ''
+  }
+  return ''
+}
+
 await setup({
   rootDir: fileURLToPath(new URL(`../fixtures/basic`, import.meta.url)),
   // overrides
   nuxtConfig: {
+    hooks: {
+      'site-config:resolve': siteConfig => {
+        siteConfig.url = getUrl()
+      }
+    },
     i18n: {
       defaultLocale: 'en'
-    },
-    site: {
-      url: () => {
-        if (process.server) {
-          // @ts-ignore
-          const headers = useRequestHeaders(['x-override-base-url'])
-          const xOverrideBaseUrl = headers['x-override-base-url']
-          console.log('xOverrideBaseUrl', xOverrideBaseUrl)
-          if (Array.isArray(xOverrideBaseUrl)) {
-            return xOverrideBaseUrl[0]
-          }
-          return xOverrideBaseUrl || ''
-        }
-        return ''
-      }
     }
   }
 })
