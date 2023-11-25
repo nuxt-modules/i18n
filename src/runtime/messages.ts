0a7399c63ae514b24c26e13da57be7621bc99b87
Bobbie Goede <bobbiegoede@gmail.com>
2023-11-25 16:18:52 +0100

    feat: `useRuntimeConfig` support (#2572)
    * feat: hacky runtime config support
    
    * refactor: remove unnecessary function passing
    
    * refactor: simplify and make typesafe

--- src/runtime/messages.ts
+++ src/runtime/messages.ts
@@ -2,12 +2,17 @@ import { deepCopy } from '@intlify/shared'
 
 import type { I18nOptions } from 'vue-i18n'
 import type { VueI18nConfig } from '../types'
+import type { NuxtApp } from 'nuxt/dist/app'
 
-export async function loadVueI18nOptions(vueI18nConfigs: VueI18nConfig[]): Promise<I18nOptions> {
+export async function loadVueI18nOptions(
+  vueI18nConfigs: VueI18nConfig[],
+  nuxt: Pick<NuxtApp, 'runWithContext'>
+): Promise<I18nOptions> {
   const vueI18nOptions: I18nOptions = { messages: {} }
   for (const configFile of vueI18nConfigs) {
     const { default: resolver } = await configFile()
-    const resolved = typeof resolver === 'function' ? await resolver() : resolver
+
+    const resolved = typeof resolver === 'function' ? await nuxt.runWithContext(async () => await resolver()) : resolver
 
     deepCopy(resolved, vueI18nOptions)
   }
