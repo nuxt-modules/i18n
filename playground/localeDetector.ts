1ea7efbf1f37e801db47e17a01afb4f66c47a87c
kazuya kawaguchi <kawakazu80@gmail.com>
2023-12-06 14:37:57 +0900

    fix: bump intlify/h3 and utilts

--- playground/localeDetector.ts
+++ playground/localeDetector.ts
@@ -3,17 +3,17 @@ import type { H3Event } from 'h3'
 // Detect based on query, cookie or header
 // not using `getXLocale` functions as they return `en-US` by default if no locale is found
 export default function (event: H3Event): string {
-  const query = getQuery(event)
-  if (query?.locale != null && typeof query.locale === 'string') {
-    return query.locale
+  const query = tryQueryLocale(event)
+  if (query) {
+    return query.toString()
   }
 
-  const cookie = getCookie(event, 'i18n_locale')
+  const cookie = tryCookieLocale(event, { name: 'i18n_locale' })
   if (cookie) {
     return cookie.toString()
   }
 
-  const header = getHeader(event, 'Accept-Language')
+  const header = tryHeaderLocale(event)
   if (header) {
     return header.toString()
   }
