f1c1d55617f5a7091df9e9317ec1ba0fe7aa1f78
kazuya kawaguchi <kawakazu80@gmail.com>
2023-12-06 15:15:36 +0900

    refactor

--- playground/localeDetector.ts
+++ playground/localeDetector.ts
@@ -2,17 +2,17 @@ import type { H3Event } from 'h3'
 
 // Detect based on query, cookie or header
 export default function (event: H3Event): string {
-  const query = tryQueryLocale(event, { lang: undefined })
+  const query = tryQueryLocale(event, { lang: '' })
   if (query) {
     return query.toString()
   }
 
-  const cookie = tryCookieLocale(event, { lang: undefined, name: 'i18n_locale' })
+  const cookie = tryCookieLocale(event, { lang: '', name: 'i18n_locale' })
   if (cookie) {
     return cookie.toString()
   }
 
-  const header = tryHeaderLocale(event, { lang: undefined })
+  const header = tryHeaderLocale(event, { lang: '' })
   if (header) {
     return header.toString()
   }
--- specs/fixtures/layers/layer-server/localeDetector.ts
+++ specs/fixtures/layers/layer-server/localeDetector.ts
@@ -1,19 +1,18 @@
 import type { H3Event } from 'h3'
 
 // Detect based on query, cookie or header
-// not using `getXLocale` functions as they return `en-US` by default if no locale is found
 export default function (event: H3Event): string {
-  const query = getQuery(event)
-  if (query?.locale != null && typeof query.locale === 'string') {
-    return query.locale
+  const query = tryQueryLocale(event, { lang: '' })
+  if (query) {
+    return query.toString()
   }
 
-  const cookie = getCookie(event, 'i18n_locale')
+  const cookie = tryCookieLocale(event, { lang: '', name: 'i18n_locale' })
   if (cookie) {
     return cookie.toString()
   }
 
-  const header = getHeader(event, 'Accept-Language')
+  const header = tryHeaderLocale(event, { lang: '' })
   if (header) {
     return header.toString()
   }
