dce5e7551c602ce4f40d22bfa7b00390f5ceda16
MuhammadM1998 <MuhammadM.1097@gmail.com>
2023-12-15 13:52:19 +0200

    chore: migrate eslint.ignore

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,20 @@ import eslintConfigPrettier from 'eslint-config-prettier'
 export default [
   // Globals
   {
+    ignores: [
+      'dist',
+      'playground',
+      'specs/fixtures',
+      'specs/utils',
+      'test/fixtures',
+      'coverage',
+      'docs/components/content/Logo.vue',
+      'docs/components/content/VoltaBoard.vue',
+      'docs/components/AppHeader.vue',
+      'docs/components/AppHeaderNavigation.vue',
+      '.eslintcache',
+      'src/runtime/templates'
+    ],
     env: { node: true },
     languageOptions: { parser: tsEsLintParser },
     parserOptions: { sourceType: 'module' }
