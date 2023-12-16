d93ea6f96817b1e58212b8ba08ae3d391a96c8fa
Muhammad Mahmoud <MuhammadM.1097@gmail.com>
2023-12-16 13:20:53 +0200

    chore: migrate to ESLint flat config (#2622)
    * chore: migrate globals
    
    -- `root: true` is not needed in flat config
    
    * chore: migrate extends
    
    * chore: migrate TS plugin
    
    * chore: migrate TS parser
    
    * chore: remove eslintrc
    
    * chore: migrate eslint.ignore
    
    * chore: migrate files extensions
    
    * fix: move globals & parserOptions to languageOptions
    
    * fix: use `FlatCompat` utility to extend  plugins correctly
    
    * fix: move ignores to top
    
    * fix: remove deprecated `--ext` flag from commands
    
    * chore: format

---
+++ eslint.config.js
@@ -0,0 +1,47 @@
+import globals from 'globals'
+import tsEsLintPlugin from '@typescript-eslint/eslint-plugin'
+import tsEsLintParser from '@typescript-eslint/parser'
+import eslintConfigPrettier from 'eslint-config-prettier'
+import { FlatCompat } from '@eslint/eslintrc'
+const compat = new FlatCompat()
+
+export default [
+  // Ignores
+  {
+    ignores: [
+      '.eslintcache/**',
+      'dist/**',
+      'playground/**',
+      'specs/fixtures/**',
+      'specs/utils/**',
+      'test/fixtures/**',
+      'coverage/**',
+      'src/runtime/templates/**',
+      'docs/components/content/Logo.vue',
+      'docs/components/content/VoltaBoard.vue',
+      'docs/components/AppHeader.vue',
+      'docs/components/AppHeaderNavigation.vue'
+    ]
+  },
+
+  // Globals
+  {
+    files: ['**/*.js', '**/*.ts', '**/*.vue', '**/*.json'],
+    languageOptions: {
+      globals: globals.node,
+      parser: tsEsLintParser,
+      parserOptions: { sourceType: 'module' }
+    }
+  },
+
+  // Extends
+  ...compat.extends('plugin:@typescript-eslint/recommended'),
+  ...compat.extends('plugin:@typescript-eslint/eslint-recommended'),
+  eslintConfigPrettier,
+
+  // TS Plugin
+  {
+    plugins: { '@typescript-eslint': tsEsLintPlugin },
+    rules: { '@typescript-eslint/ban-ts-comment': 'off' }
+  }
+]
