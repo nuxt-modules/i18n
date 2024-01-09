a763e6216793739e010083522da19090658ae342
Rafal Chlodnicki <rchl2k@gmail.com>
2024-01-09 23:41:46 +0100

    longer timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
-    hookTimeout: 30000,
+    hookTimeout: 60000,
     // Disable stderr in tests to silence the noise.
     onConsoleLog (_log, type) {
       if (type === 'stderr') {
         return false
       }
-    },
-  },
+    }
+  }
 })
