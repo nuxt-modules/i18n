955cff3020463ad821ea017b0342f37d23d6a198
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-01-30 18:48:21 +0200

    chore: cleanup

--- docs/layouts/docs.vue
+++ docs/layouts/docs.vue
@@ -2,23 +2,24 @@
 // @ts-expect-error This is because we're using Nuxt Content v2.8.2 instead of the new version which includes these types. We're using the old version because the latest has issues with highlighting
 import type { NavItem } from '@nuxt/content/dist/runtime/types'
 
+// Get navigation tree relative to the '/content/docs'
 const navigation = inject<Ref<NavItem[]>>('navigation')
-
-// Show only v7 navigation content on v7 Page
-// And show all navigation content except v7 navigation on other docs pages
-const route = useRoute()
 const { navPageFromPath } = useContentHelpers()
-
-const isV7DocsPage = computed(() => route.path.includes('/v7'))
-const mappedNavigation = computed(() =>
+const allNavigationTree = computed(() =>
   mapContentNavigation(navPageFromPath('/docs', navigation.value)?.children || [])
 )
-const v7DocsNavItemIndex = computed(() => mappedNavigation.value.findIndex(el => el.to.toString().includes('/v7')))
-const navigationTree = computed(() => {
-  return mappedNavigation.value.filter((_el, index) =>
-    isV7DocsPage.value ? index === v7DocsNavItemIndex.value : index !== v7DocsNavItemIndex.value
+
+// Detect if we're on the v7 docs
+const route = useRoute()
+const isV7Docs = computed(() => route.path.includes('/v7'))
+
+// Exclude the v7 docs from the navigation tree if we're not on the v7 docs, and vice versa
+const v7DocsNavItemIndex = computed(() => allNavigationTree.value.findIndex(el => el.to.toString().includes('/v7')))
+const activeNavigationTree = computed(() =>
+  allNavigationTree.value.filter((_el, index) =>
+    isV7Docs.value ? index === v7DocsNavItemIndex.value : index !== v7DocsNavItemIndex.value
   )
-})
+)
 </script>
 
 <template>
@@ -28,7 +29,7 @@ const navigationTree = computed(() => {
         <UAside>
           <VersionSelect />
           <UDivider type="dashed" class="mb-6" />
-          <UNavigationTree :links="mapContentNavigation(navigationTree)" default-open :multiple="false" />
+          <UNavigationTree :links="mapContentNavigation(activeNavigationTree)" default-open :multiple="false" />
         </UAside>
       </template>
 
