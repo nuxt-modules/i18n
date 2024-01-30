b17d398db7f56b0710050562334aed0b490715e8
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-01-30 18:53:49 +0200

    refactor: cleanup

--- docs/pages/roadmap.vue
+++ docs/pages/roadmap.vue
@@ -1,6 +1,7 @@
 <script setup>
+// SEO & OG
 const title = 'Roadmap'
-const description = 'Discover our Volta board for @nuxt/ui development status.'
+const description = 'Discover our Volta board for @nuxt/i18n development status.'
 
 useSeoMeta({
   titleTemplate: '%s - Nuxt i18n',
@@ -16,22 +17,22 @@ defineOgImage({
   description
 })
 
-const appConfig = useAppConfig()
-const colorMode = useColorMode()
-
-const token =
+// Volta board
+const VOLTA_TOKEN =
   'eyJzdGF0dXNlcyI6WyJ0cmlhZ2UiLCJiYWNrbG9nIiwidG9kbyIsImluX3Byb2dyZXNzIiwiaW5fcmV2aWV3IiwiZG9uZSIsInJlbGVhc2VkIiwiY2FuY2VsbGVkIl0sImZpbHRlcnMiOnt9LCJvd25lciI6Im51eHQtbW9kdWxlcyIsIm5hbWUiOiJpMThuIn0='
+const colorMode = useColorMode()
+const appConfig = useAppConfig()
 
-const src = computed(
-  () =>
-    `https://volta.net/embed/${token}?theme=${colorMode.value}&gray=${appConfig.ui.gray}&primary=${appConfig.ui.primary}`
-)
+const voltaBoardSrc = computed(() => {
+  const voltaBaseUrl = `https://volta.net/embed/${VOLTA_TOKEN}`
+  return `${voltaBaseUrl}?theme=${colorMode.value}&primary=${appConfig.ui.primary}&gray=${appConfig.ui.gray}`
+})
 </script>
 
 <template>
   <div class="h-[calc(100vh-var(--header-height)-var(--header-height)-1px)]">
     <ClientOnly>
-      <iframe :src="src" width="100%" height="100%" />
+      <iframe :src="voltaBoardSrc" width="100%" height="100%" />
     </ClientOnly>
   </div>
 </template>
