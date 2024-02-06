7e035222b18bd55e6a898d3ec0e6fca29def463c
MuhammadM1998 <MuhammadM.1097@gmail.com>
2024-02-06 13:55:04 +0200

    chore: organize a bit

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,7 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   extends: ['@nuxt/ui-pro'],
   modules: ['@nuxt/content', '@nuxt/ui', '@nuxtjs/fontaine', '@nuxtjs/google-fonts', 'nuxt-og-image'],
+  routeRules: {
+    '/api/search.json': { prerender: true }
+  },
+
+  // Nuxt UI & UI Pro
+  ui: { icons: ['heroicons', 'simple-icons'] },
+  uiPro: { license: 'oss' }, // special license for nuxt & nuxt-modules orgs
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
     'components:extend': components => {
@@ -10,30 +16,15 @@ export default defineNuxtConfig({
       globals.forEach(c => (c.global = true))
     }
   },
-  ui: {
-    icons: ['heroicons', 'simple-icons']
-  },
-  uiPro: {
-    license: 'oss' // special license for nuxt & nuxt-modules orgs
-  },
+
   // Fonts
-  fontMetrics: {
-    fonts: ['DM Sans']
-  },
+  fontMetrics: { fonts: ['DM Sans'] },
   googleFonts: {
     display: 'swap',
     download: true,
-    families: {
-      'DM+Sans': [400, 500, 600, 700]
-    }
+    families: { 'DM+Sans': [400, 500, 600, 700] }
   },
-  routeRules: {
-    '/api/search.json': { prerender: true }
-  },
-  devtools: {
-    enabled: true
-  },
-  typescript: {
-    strict: false
-  }
+
+  devtools: { enabled: true },
+  typescript: { strict: false }
 })
