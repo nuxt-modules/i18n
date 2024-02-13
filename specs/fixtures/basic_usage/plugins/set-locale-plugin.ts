61d542884cb02d49d66bb496d8578a06e7f07476
Bobbie Goede <bobbiegoede@gmail.com>
2024-02-13 15:23:52 +0100

    fix: `setLocale` throws error updating locale cookie from plugin (#2783)

--- specs/fixtures/basic_usage/plugins/set-locale-plugin.ts
+++
@@ -1,8 +0,0 @@
-import { defineNuxtPlugin } from '#imports'
-
-export default defineNuxtPlugin(async nuxtApp => {
-  if ('pluginSetLocale' in nuxtApp._route.query) {
-    const app = useNuxtApp()
-    await app.$i18n.setLocale('fr')
-  }
-})
