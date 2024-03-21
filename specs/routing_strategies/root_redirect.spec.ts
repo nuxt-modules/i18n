a69bbeedebd6aff593d2696a2ea85ac79a39da51
Bobbie Goede <bobbiegoede@gmail.com>
2024-03-21 14:23:56 +0100

    test: merge issue #2758 test

--- specs/routing_strategies/root_redirect.spec.ts
+++ specs/routing_strategies/root_redirect.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from 'vitest'
 import { fileURLToPath } from 'node:url'
 import { setup, url, fetch } from '../utils'
+import { startServerWithRuntimeConfig } from '../helper'
 
 await setup({
   rootDir: fileURLToPath(new URL(`../fixtures/basic`, import.meta.url)),
@@ -10,12 +11,41 @@ await setup({
     i18n: {
       strategy: 'prefix',
       defaultLocale: 'en',
-      rootRedirect: 'fr'
+      // configure `rootDirect` to object so it can be overwritten by `runtimeConfig`
+      rootRedirect: { path: 'about', statusCode: 302 }
     }
   }
 })
 
-test('can redirect to rootRedirect option path', async () => {
-  const res = await fetch('/')
-  expect(res.url).toBe(url('/fr'))
+describe('rootRedirect', async () => {
+  test('can redirect to rootRedirect option path', async () => {
+    const restore = await startServerWithRuntimeConfig({
+      public: {
+        i18n: {
+          rootRedirect: 'fr'
+        }
+      }
+    })
+
+    const res = await fetch('/')
+    expect(res.url).toBe(url('/fr'))
+
+    await restore()
+  })
+
+  test('(#2758) `statusCode` in `rootRedirect` should work with strategy "prefix"', async () => {
+    const restore = await startServerWithRuntimeConfig({
+      public: {
+        i18n: {
+          rootRedirect: { statusCode: 418, path: 'test-route' }
+        }
+      }
+    })
+
+    const res = await fetch(url('/'))
+    expect(res.status).toEqual(418)
+    expect(res.headers.get('location')).toEqual('/en/test-route')
+
+    await restore()
+  })
 })
