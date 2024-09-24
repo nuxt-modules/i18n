d0673365f7b6c40e776bfa72dfb72b6a2a19e49e
Bobbie Goede <bobbiegoede@gmail.com>
2024-09-24 22:51:38 +0200

    refactor: simplify `createLocaleFromRouteGetter` (#3127)

--- src/runtime/routing/extends/router.ts
+++ src/runtime/routing/extends/router.ts
@@ -1,49 +1,41 @@
-import { isString, isObject } from '@intlify/shared'
-import { getLocalesRegex } from '../utils'
+import { getLocalesRegex, getRouteName } from '../utils'
 import { localeCodes } from '#build/i18n.options.mjs'
+import { useRuntimeConfig } from '#imports'
 
 import type { RouteLocationNormalized, RouteLocationNormalizedLoaded } from 'vue-router'
-import { useRuntimeConfig } from '#imports'
+
+const localesPattern = `(${localeCodes.join('|')})`
+const regexpPath = getLocalesRegex(localeCodes)
 
 export type GetLocaleFromRouteFunction = (
   route: RouteLocationNormalizedLoaded | RouteLocationNormalized | string
 ) => string
+
 export function createLocaleFromRouteGetter() {
   const { routesNameSeparator, defaultLocaleRouteNameSuffix } = useRuntimeConfig().public.i18n
-  const localesPattern = `(${localeCodes.join('|')})`
   const defaultSuffixPattern = `(?:${routesNameSeparator}${defaultLocaleRouteNameSuffix})?`
   const regexpName = new RegExp(`${routesNameSeparator}${localesPattern}${defaultSuffixPattern}$`, 'i')
-  const regexpPath = getLocalesRegex(localeCodes)
 
   /**
-   * extract locale code from given route:
-   * - if route has a name, try to extract locale from it
-   * - otherwise, fall back to using the routes'path
+   * extract locale code from route name or path
    */
   const getLocaleFromRoute: GetLocaleFromRouteFunction = route => {
-    // extract from route name
-    if (isObject(route)) {
-      if (route.name) {
-        const name = isString(route.name) ? route.name : route.name.toString()
-        const matches = name.match(regexpName)
-        if (matches && matches.length > 1) {
-          return matches[1]
-        }
-      } else if (route.path) {
-        // Extract from path
-        const matches = route.path.match(regexpPath)
-        if (matches && matches.length > 1) {
-          return matches[1]
-        }
-      }
-    } else if (isString(route)) {
-      const matches = route.match(regexpPath)
-      if (matches && matches.length > 1) {
-        return matches[1]
-      }
+    let matches: RegExpMatchArray | null = null
+
+    if (typeof route === 'string') {
+      matches = route.match(regexpPath)
+      return matches?.[1] ?? ''
     }
 
-    return ''
+    if (route.name) {
+      // extract from route name
+      matches = getRouteName(route.name).match(regexpName)
+    } else if (route.path) {
+      // extract from path
+      matches = route.path.match(regexpPath)
+    }
+
+    return matches?.[1] ?? ''
   }
 
   return getLocaleFromRoute
