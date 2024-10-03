5a082d26770e3d1afbb81eb2e02aadc6da8bc07f
Bobbie Goede <bobbiegoede@gmail.com>
2024-10-03 23:24:57 +0200

    fix: always configure nitro temporarily

--- src/nitro.ts
+++ src/nitro.ts
@@ -37,80 +37,80 @@ export async function setupNitro(
   const [enableServerIntegration, localeDetectionPath] = await resolveLocaleDetectorPath(nuxt)
 
   nuxt.hook('nitro:config', async nitroConfig => {
-    if (enableServerIntegration) {
-      const additionalParams: AdditionalSetupNitroParams = {
-        optionsCode: genTemplate(true, true),
-        localeInfo
-      }
-      // inline module runtime in Nitro bundle
-      nitroConfig.externals = defu(typeof nitroConfig.externals === 'object' ? nitroConfig.externals : {}, {
-        inline: [resolver.resolve('./runtime')]
-      })
+    // if (enableServerIntegration) {
+    const additionalParams: AdditionalSetupNitroParams = {
+      optionsCode: genTemplate(true, true),
+      localeInfo
+    }
+    // inline module runtime in Nitro bundle
+    nitroConfig.externals = defu(typeof nitroConfig.externals === 'object' ? nitroConfig.externals : {}, {
+      inline: [resolver.resolve('./runtime')]
+    })
 
-      // install server resource transform plugin for yaml / json5 format
-      nitroConfig.rollupConfig = nitroConfig.rollupConfig || {}
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises -- FIXME
-      nitroConfig.rollupConfig.plugins = (await nitroConfig.rollupConfig.plugins) || []
-      // eslint-disable-next-line @typescript-eslint/no-floating-promises -- FIXME
-      nitroConfig.rollupConfig.plugins = Array.isArray(nitroConfig.rollupConfig.plugins)
-        ? nitroConfig.rollupConfig.plugins
-        : [nitroConfig.rollupConfig.plugins]
+    // install server resource transform plugin for yaml / json5 format
+    nitroConfig.rollupConfig = nitroConfig.rollupConfig || {}
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises -- FIXME
+    nitroConfig.rollupConfig.plugins = (await nitroConfig.rollupConfig.plugins) || []
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises -- FIXME
+    nitroConfig.rollupConfig.plugins = Array.isArray(nitroConfig.rollupConfig.plugins)
+      ? nitroConfig.rollupConfig.plugins
+      : [nitroConfig.rollupConfig.plugins]
 
-      // @ts-ignore NOTE: A type error occurs due to a mismatch between Vite plugins and those of Rollup
-      nitroConfig.rollupConfig.plugins.push(i18nVirtualLoggerPlugin(nuxtOptions.debug))
+    // @ts-ignore NOTE: A type error occurs due to a mismatch between Vite plugins and those of Rollup
+    nitroConfig.rollupConfig.plugins.push(i18nVirtualLoggerPlugin(nuxtOptions.debug))
 
-      const yamlPaths = getResourcePaths(additionalParams.localeInfo, /\.ya?ml$/)
-      if (yamlPaths.length > 0) {
-        // @ts-ignore NOTE: A type error occurs due to a mismatch between the version of rollup on the nitro side (v4.x) and the version of rollup that `@rollup/plugin-yaml` depends on (v3.x). We ignore this type error because `@rollup/plugin-yaml` is rollup version compatible.
-        nitroConfig.rollupConfig.plugins.push(yamlPlugin({ include: yamlPaths }))
-      }
-      const json5Paths = getResourcePaths(additionalParams.localeInfo, /\.json5?$/)
-      if (json5Paths.length > 0) {
-        // @ts-ignore NOTE: A type error occurs due to a mismatch between the version of rollup on the nitro side (v4.x) and the version of rollup that `@miyaneee/rollup-plugin-json5` depends on (v3.x). We ignore this type error because `@miyaneee/rollup-plugin-json5` is rollup version compatible.
-        nitroConfig.rollupConfig.plugins.push(json5Plugin({ include: json5Paths }))
-      }
+    const yamlPaths = getResourcePaths(additionalParams.localeInfo, /\.ya?ml$/)
+    if (yamlPaths.length > 0) {
+      // @ts-ignore NOTE: A type error occurs due to a mismatch between the version of rollup on the nitro side (v4.x) and the version of rollup that `@rollup/plugin-yaml` depends on (v3.x). We ignore this type error because `@rollup/plugin-yaml` is rollup version compatible.
+      nitroConfig.rollupConfig.plugins.push(yamlPlugin({ include: yamlPaths }))
+    }
+    const json5Paths = getResourcePaths(additionalParams.localeInfo, /\.json5?$/)
+    if (json5Paths.length > 0) {
+      // @ts-ignore NOTE: A type error occurs due to a mismatch between the version of rollup on the nitro side (v4.x) and the version of rollup that `@miyaneee/rollup-plugin-json5` depends on (v3.x). We ignore this type error because `@miyaneee/rollup-plugin-json5` is rollup version compatible.
+      nitroConfig.rollupConfig.plugins.push(json5Plugin({ include: json5Paths }))
+    }
 
-      nitroConfig.virtual = nitroConfig.virtual || {}
-      nitroConfig.virtual['#internal/i18n/options.mjs'] = () => additionalParams.optionsCode
-      nitroConfig.virtual['#internal/i18n/locale.detector.mjs'] = () => `
+    nitroConfig.virtual = nitroConfig.virtual || {}
+    nitroConfig.virtual['#internal/i18n/options.mjs'] = () => additionalParams.optionsCode
+    nitroConfig.virtual['#internal/i18n/locale.detector.mjs'] = () => `
 import localeDetector from ${JSON.stringify(localeDetectionPath)}
 export { localeDetector }
 `
 
-      // auto import for server-side
-      if (nitroConfig.imports) {
-        nitroConfig.imports.presets = nitroConfig.imports.presets || []
-        // `@intlify/h3` utilities
-        nitroConfig.imports.presets.push({
-          from: H3_PKG,
-          imports: ['useTranslation']
-        })
-        const h3UtilsExports = await resolveModuleExportNames(UTILS_H3_PKG, { url: import.meta.url })
-        // `@intlify/utils/h3`, `defineI18nLocale` and `defineI18nConfig`
-        nitroConfig.imports.imports = nitroConfig.imports.imports || []
-        nitroConfig.imports.imports.push(
+    // auto import for server-side
+    if (nitroConfig.imports) {
+      nitroConfig.imports.presets = nitroConfig.imports.presets || []
+      // `@intlify/h3` utilities
+      nitroConfig.imports.presets.push({
+        from: H3_PKG,
+        imports: ['useTranslation']
+      })
+      const h3UtilsExports = await resolveModuleExportNames(UTILS_H3_PKG, { url: import.meta.url })
+      // `@intlify/utils/h3`, `defineI18nLocale` and `defineI18nConfig`
+      nitroConfig.imports.imports = nitroConfig.imports.imports || []
+      nitroConfig.imports.imports.push(
+        ...[
+          ...h3UtilsExports.map(key => ({
+            name: key,
+            as: key,
+            from: resolver.resolve(nuxt.options.alias[UTILS_H3_PKG])
+          })),
+          ...[NUXT_I18N_COMPOSABLE_DEFINE_LOCALE, NUXT_I18N_COMPOSABLE_DEFINE_CONFIG].map(key => ({
+            name: key,
+            as: key,
+            from: resolver.resolve('runtime/composables/shared')
+          })),
           ...[
-            ...h3UtilsExports.map(key => ({
-              name: key,
-              as: key,
-              from: resolver.resolve(nuxt.options.alias[UTILS_H3_PKG])
-            })),
-            ...[NUXT_I18N_COMPOSABLE_DEFINE_LOCALE, NUXT_I18N_COMPOSABLE_DEFINE_CONFIG].map(key => ({
-              name: key,
-              as: key,
-              from: resolver.resolve('runtime/composables/shared')
-            })),
-            ...[
-              {
-                name: NUXT_I18N_COMPOSABLE_DEFINE_LOCALE_DETECTOR,
-                as: NUXT_I18N_COMPOSABLE_DEFINE_LOCALE_DETECTOR,
-                from: resolver.resolve('runtime/composables/server')
-              }
-            ]
+            {
+              name: NUXT_I18N_COMPOSABLE_DEFINE_LOCALE_DETECTOR,
+              as: NUXT_I18N_COMPOSABLE_DEFINE_LOCALE_DETECTOR,
+              from: resolver.resolve('runtime/composables/server')
+            }
           ]
-        )
-      }
+        ]
+      )
     }
+    // }
 
     nitroConfig.replace = nitroConfig.replace || {}
 
