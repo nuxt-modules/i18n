b64eadee8416ba2a43f131a4300a7e858bb0cd8d
userquin <userquin@gmail.com>
2024-11-10 11:07:32 +0100

    chore: add types to build config file

--- build.config.ts
+++ build.config.ts
@@ -1,5 +1,10 @@
 import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
+  entries: [
+    'src/module',
+    // Chunking
+    'src/types'
+  ],
   externals: ['node:fs', 'node:url', 'webpack', '@babel/parser', 'unplugin-vue-router', 'unplugin-vue-router/options']
 })
