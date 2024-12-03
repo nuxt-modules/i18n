7f7eaf2edfd7d602e1bddc89dab6ae423a980f0e
Dotwood Media <info@dotwood.media>
2024-12-03 17:22:09 +0100

    fix: default language multi domain locales (#3250)
    Co-authored-by: Vincent de Vreede <vincentdevreede@live.nl>
    Co-authored-by: Dotwood Media <info@dotwood.media>

---
+++ specs/fixtures/multi_domains_locales/pages/parent.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <p id="parent-text">{{ t('parent.text') }}</p>
+  <NuxtPage />
+</template>
---
+++ specs/fixtures/multi_domains_locales/pages/parent/child.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
+
+<template>
+  <p id="child-text">{{ t('parent.child.text') }}</p>
+</template>
