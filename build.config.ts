e011b80837a272e811195e4d4e1ec1e08698cb3a
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-02 00:40:42 +0100

    fix: mark rspack as external

--- build.config.ts
+++ build.config.ts
@@ -2,5 +2,13 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: ['src/module'],
-  externals: ['node:fs', 'node:url', 'webpack', '@babel/parser', 'unplugin-vue-router', 'unplugin-vue-router/options']
+  externals: [
+    'node:fs',
+    'node:url',
+    'webpack',
+    '@rspack/core',
+    '@babel/parser',
+    'unplugin-vue-router',
+    'unplugin-vue-router/options'
+  ]
 })
