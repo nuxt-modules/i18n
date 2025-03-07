e9b760ea63296488e5887016683e4d31b5934256
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-07 13:59:52 +0100

    fix: use `@oxc-parser/wasm` as parser fallback (#3391)

--- src/utils/parse.ts
+++ src/utils/parse.ts
@@ -1,11 +1,20 @@
-import { provider } from 'std-env'
+// Adapted from https://github.com/nuxt/nuxt/blob/6d85c15fb1783b003bb5d7cdefdd22b54f871f9d/packages/nuxt/src/core/utils/parse.ts
 
 export let parseSync: typeof import('oxc-parser').parseSync
 
 export async function initParser() {
-  parseSync = await (
-    provider === 'stackblitz'
-      ? (import('@oxc-parser/wasm') as unknown as Promise<typeof import('oxc-parser')>)
-      : import('oxc-parser')
-  ).then(r => r.parseSync)
+  try {
+    parseSync = await import('oxc-parser').then(r => r.parseSync)
+  } catch (_) {
+    console.warn('[nuxt-i18n]: Unable to import `oxc-parser`, falling back to `@oxc-parser/wasm`.')
+
+    const { parseSync: parse } = await import('@oxc-parser/wasm')
+    parseSync = (filename, sourceText, options) =>
+      // @ts-expect-error sourceType property conflict
+      parse(sourceText, {
+        ...(options || {}),
+        sourceFilename: filename.replace(/\?.*$/, '') + `.${options?.lang || 'ts'}`,
+        sourceType: 'module'
+      })
+  }
 }
