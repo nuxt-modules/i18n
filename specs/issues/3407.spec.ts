19fe0f47e33f8918cdfd892824554607d23a49a1
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-21 09:12:56 +0100

    test: improve test description

--- specs/issues/3407.spec.ts
+++ specs/issues/3407.spec.ts
@@ -1,22 +1,24 @@
-import { test, expect } from 'vitest'
+import { test, expect, describe } from 'vitest'
 import { fileURLToPath } from 'node:url'
 import { createPage, setup, url } from '../utils'
 import { getText } from '../helper'
 
-// this is an SSG test
-await setup({
-  rootDir: fileURLToPath(new URL(`../fixtures/issues/3407`, import.meta.url)),
-  browser: true,
-  prerender: true,
-  port: [7777, 7776]
-})
+describe('#3407', async () => {
+  await setup({
+    rootDir: fileURLToPath(new URL(`../fixtures/issues/3407`, import.meta.url)),
+    browser: true,
+    // this is an SSG test
+    prerender: true,
+    port: [7777, 7776]
+  })
 
-test('does not reset cookie no refresh', async () => {
-  const page = await createPage('/')
-  const heading = await getText(page, '#translated-heading')
-  expect(heading).toEqual(`Problema de i18n SSG`)
+  test('`detectBrowserLanguage: false` disables locale detection in SSG', async () => {
+    const page = await createPage('/')
+    const heading = await getText(page, '#translated-heading')
+    expect(heading).toEqual(`Problema de i18n SSG`)
 
-  await page.goto(url('/', 7776))
-  const heading2 = await getText(page, '#translated-heading')
-  expect(heading2).toEqual(`i18n SSG issue`)
+    await page.goto(url('/', 7776))
+    const heading2 = await getText(page, '#translated-heading')
+    expect(heading2).toEqual(`i18n SSG issue`)
+  })
 })
