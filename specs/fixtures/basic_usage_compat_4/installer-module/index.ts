d57882f78158a39c290df101d70db7b3323ae216
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-22 12:57:02 +0100

    test: await module installations in fixtures

--- specs/fixtures/basic_usage_compat_4/installer-module/index.ts
+++ specs/fixtures/basic_usage_compat_4/installer-module/index.ts
@@ -4,7 +4,7 @@ export default defineNuxtModule({
   async setup(options, nuxt) {
     const { resolve } = createResolver(import.meta.url)
 
-    installModule('@nuxtjs/i18n', {
+    await installModule('@nuxtjs/i18n', {
       langDir: resolve('./locales'),
       vueI18n: resolve('./i18n.config.ts'),
       locales: [
