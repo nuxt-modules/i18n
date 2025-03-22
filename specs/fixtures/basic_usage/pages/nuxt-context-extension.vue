dcf297b0149123640935f760b395e7601f34f466
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-22 10:57:14 +0100

    test: assert `getRouteBaseName` accepts route name string

--- specs/fixtures/basic_usage/pages/nuxt-context-extension.vue
+++ specs/fixtures/basic_usage/pages/nuxt-context-extension.vue
@@ -3,6 +3,7 @@
 <template>
   <div>
     <p id="get-route-base-name">{{ $nuxt.$getRouteBaseName($route) }}</p>
+    <p id="get-route-base-name-string">{{ $nuxt.$getRouteBaseName($route.name) }}</p>
     <p id="switch-locale-path">{{ $nuxt.$switchLocalePath('ja') }}</p>
     <p id="locale-path">{{ $nuxt.$localePath('nuxt-context-extension', 'nl') }}</p>
     <p id="locale-route">{{ JSON.stringify($nuxt.$localeRoute()) }}</p>
