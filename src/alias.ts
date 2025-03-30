125b5c85fa3a465072b9e7a0345828c73acad6b4
Bobbie Goede <bobbiegoede@gmail.com>
2025-03-30 16:58:29 +0200

    fix: replace deprecated `tryResolveModule` with `resolveModule`

--- src/alias.ts
+++ src/alias.ts
@@ -1,4 +1,4 @@
-import { tryResolveModule } from '@nuxt/kit'
+import { directoryToURL, resolveModule } from '@nuxt/kit'
 import createDebug from 'debug'
 import {
   VUE_I18N_PKG,
@@ -17,26 +17,29 @@ import type { I18nNuxtContext } from './context'
 
 const debug = createDebug('@nuxtjs/i18n:alias')
 
-export async function setupAlias({ userOptions: options, isDev, isPrepare }: I18nNuxtContext, nuxt: Nuxt) {
-  const runtimeOnly = options.bundle?.runtimeOnly
-  const modules: Record<string, string> = {}
+export function setupAlias({ userOptions: options, isDev, isPrepare }: I18nNuxtContext, nuxt: Nuxt) {
+  const modules = {
+    [VUE_I18N_PKG]:
+      isDev || isPrepare
+        ? `${VUE_I18N_PKG}/dist/vue-i18n.mjs`
+        : `${VUE_I18N_PKG}/dist/vue-i18n${options.bundle?.runtimeOnly ? '.runtime' : ''}.mjs`,
+    [SHARED_PKG]: `${SHARED_PKG}/dist/shared.mjs`,
+    [MESSAGE_COMPILER_PKG]: `${MESSAGE_COMPILER_PKG}/dist/message-compiler.mjs`,
+    [CORE_BASE_PKG]: `${CORE_BASE_PKG}/dist/core-base.mjs`,
+    [CORE_PKG]: `${CORE_PKG}/dist/core.node.mjs`,
+    [UTILS_H3_PKG]: `${UTILS_PKG}/dist/h3.mjs`, // for `@intlify/utils/h3`
+    [UFO_PKG]: UFO_PKG
+  } as const
 
-  modules[VUE_I18N_PKG] =
-    isDev || isPrepare
-      ? `${VUE_I18N_PKG}/dist/vue-i18n.mjs`
-      : `${VUE_I18N_PKG}/dist/vue-i18n${runtimeOnly ? '.runtime' : ''}.mjs`
-  modules[SHARED_PKG] = `${SHARED_PKG}/dist/shared.mjs`
-  modules[MESSAGE_COMPILER_PKG] = `${MESSAGE_COMPILER_PKG}/dist/message-compiler.mjs`
-  modules[CORE_BASE_PKG] = `${CORE_BASE_PKG}/dist/core-base.mjs`
-  modules[CORE_PKG] = `${CORE_PKG}/dist/core.node.mjs`
-  modules[UTILS_H3_PKG] = `${UTILS_PKG}/dist/h3.mjs` // for `@intlify/utils/h3`
-  modules[UFO_PKG] = UFO_PKG
-
-  const moduleDirs: string[] = nuxt.options.modulesDir || []
-  const enhancedModulesDirs = [...moduleDirs, ...moduleDirs.map(dir => `${dir}/${NUXT_I18N_MODULE_ID}/node_modules`)]
+  const moduleDirs = ([] as string[])
+    .concat(
+      nuxt.options.modulesDir,
+      nuxt.options.modulesDir.map(dir => `${dir}/${NUXT_I18N_MODULE_ID}/node_modules`)
+    )
+    .map(x => directoryToURL(x))
 
   for (const [moduleName, moduleFile] of Object.entries(modules)) {
-    const module = await tryResolveModule(moduleFile, enhancedModulesDirs)
+    const module = resolveModule(moduleFile, { url: moduleDirs })
     if (!module) throw new Error(`Could not resolve module "${moduleFile}"`)
     nuxt.options.alias[moduleName] = module
     nuxt.options.build.transpile.push(moduleName)
