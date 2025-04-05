d967ff53c8143150f13410295200991cdb673004
Bobbie Goede <bobbiegoede@gmail.com>
2025-04-05 21:44:18 +0200

    docs: remove legacy documentation (#3527)
    * docs: remove legacy documentation
    
    * docs: remove legacy documentation

--- docs/composables/usePageMeta.ts
+++ docs/composables/usePageMeta.ts
@@ -8,7 +8,7 @@ export default function usePageMeta() {
   const route = useRoute()
   function setPageMeta({ title = '', description = '', headline = '' }: PageMeta) {
     useSeoMeta({ title, ogTitle: title, description, ogDescription: description })
-    defineOgImageComponent(/\/docs\/v[7-8]/.test(route.path) ? 'Docs' : 'DocsV9', { title, description, headline })
+    defineOgImageComponent('Docs', { title, description, headline })
   }
 
   return { setPageMeta }
