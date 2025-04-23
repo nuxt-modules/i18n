c4f5b67511e85cc47207f6c563a591b46861b882
Bobbie Goede <bobbiegoede@gmail.com>
2025-04-24 01:50:25 +0200

    refactor: move `runWithContext` mock to messages logic

--- src/runtime/server/plugin.ts
+++ src/runtime/server/plugin.ts
@@ -3,7 +3,6 @@ import { defineI18nMiddleware } from '@intlify/h3'
 import { localeCodes, vueI18nConfigs, localeLoaders } from '#internal/i18n/options.mjs'
 import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin'
 import { localeDetector as _localeDetector } from '#internal/i18n/locale.detector.mjs'
-import { nuxtMock } from './utils'
 import { loadVueI18nOptions, loadInitialMessages, makeFallbackLocaleCodes, loadAndSetLocaleMessages } from '../messages'
 
 import type { H3Event } from 'h3'
@@ -13,7 +12,7 @@ import type { CoreContext } from '@intlify/h3'
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 export default defineNitroPlugin(async nitro => {
   // `defineI18nMiddleware` options (internally, options passed to`createCoreContext` in intlify / core) are compatible with vue-i18n options
-  const options = await loadVueI18nOptions(vueI18nConfigs, nuxtMock)
+  const options = await loadVueI18nOptions(vueI18nConfigs)
   options.messages = options.messages || {}
   const fallbackLocale = (options.fallbackLocale = options.fallbackLocale ?? false)
 
@@ -21,18 +20,13 @@ export default defineNitroPlugin(async nitro => {
   const initialLocale = runtimeI18n.defaultLocale || options.locale || 'en-US'
 
   // load initial locale messages for intlify/h3
-  options.messages = await loadInitialMessages(
-    options.messages,
-    localeLoaders,
-    {
-      localeCodes,
-      initialLocale,
-      lazy: runtimeI18n.lazy,
-      defaultLocale: runtimeI18n.defaultLocale,
-      fallbackLocale: options.fallbackLocale
-    },
-    nuxtMock
-  )
+  options.messages = await loadInitialMessages(options.messages, localeLoaders, {
+    localeCodes,
+    initialLocale,
+    lazy: runtimeI18n.lazy,
+    defaultLocale: runtimeI18n.defaultLocale,
+    fallbackLocale: options.fallbackLocale
+  })
 
   const localeDetector = async (
     event: H3Event,
@@ -46,10 +40,10 @@ export default defineNitroPlugin(async nitro => {
       if (fallbackLocale) {
         const fallbackLocales = makeFallbackLocaleCodes(fallbackLocale, [locale])
         await Promise.all(
-          fallbackLocales.map(locale => loadAndSetLocaleMessages(locale, localeLoaders, i18nContext.messages, nuxtMock))
+          fallbackLocales.map(locale => loadAndSetLocaleMessages(locale, localeLoaders, i18nContext.messages))
         )
       }
-      await loadAndSetLocaleMessages(locale, localeLoaders, i18nContext.messages, nuxtMock)
+      await loadAndSetLocaleMessages(locale, localeLoaders, i18nContext.messages)
     }
     return locale
   }
