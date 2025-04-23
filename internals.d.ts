78f268ca441e1f79e9333c60ed05d52969acaef6
Bobbie Goede <bobbiegoede@gmail.com>
2025-04-24 01:24:01 +0200

    fix: improve types for internal virtual files (#3565)

--- internals.d.ts
+++ internals.d.ts
@@ -1,56 +1,36 @@
 declare module '#build/i18n.options.mjs' {
-  import type { DeepRequired } from 'ts-essentials'
-  import type { LocaleObject, NuxtI18nOptions, VueI18nConfig } from '@nuxtjs/i18n'
+  import type { LocaleObject, VueI18nConfig } from '@nuxtjs/i18n'
 
   export type { LocaleObject }
 
-  type LocaleLoader = {
-    key: string
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    load: () => Promise<any>
-    cache: boolean
-  }
-
+  type LocaleLoader = { key: string; cache: boolean; load: () => Promise<never> }
   export const localeLoaders: Record<string, LocaleLoader[]>
-
   export const vueI18nConfigs: VueI18nConfig[]
-
   export const localeCodes: string[]
-  export const nuxtI18nOptions: DeepRequired<NuxtI18nOptions<Context>>
   export const normalizedLocales: LocaleObject[]
-  export const isSSG = false
+  export const isSSG: boolean
   export const hasPages: boolean
   export const parallelPlugin: boolean
 
-  export const NUXT_I18N_MODULE_ID = ''
+  export const NUXT_I18N_MODULE_ID: string
   export const DYNAMIC_PARAMS_KEY: string
   export const DEFAULT_COOKIE_KEY: string
   export const SWITCH_LOCALE_PATH_LINK_IDENTIFIER: string
 }
 
 declare module '#internal/i18n/options.mjs' {
-  import type { DeepRequired } from 'ts-essentials'
-  import type { LocaleObject, NuxtI18nOptions, VueI18nConfig } from '@nuxtjs/i18n'
+  import type { LocaleObject, VueI18nConfig } from '@nuxtjs/i18n'
 
-  type LocaleLoader = {
-    key: string
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    load: () => Promise<any>
-    cache: boolean
-  }
-
+  type LocaleLoader = { key: string; cache: boolean; load: () => Promise<never> }
   export const localeLoaders: Record<string, LocaleLoader[]>
-
   export const vueI18nConfigs: VueI18nConfig[]
-
   export const localeCodes: string[]
-  export const nuxtI18nOptions: DeepRequired<NuxtI18nOptions<Context>>
   export const normalizedLocales: LocaleObject[]
-  export const isSSG = false
+  export const isSSG: boolean
   export const hasPages: boolean
   export const parallelPlugin: boolean
 
-  export const NUXT_I18N_MODULE_ID = ''
+  export const NUXT_I18N_MODULE_ID: string
   export const DYNAMIC_PARAMS_KEY: string
   export const DEFAULT_COOKIE_KEY: string
   export const SWITCH_LOCALE_PATH_LINK_IDENTIFIER: string
@@ -60,6 +40,10 @@ declare module '#internal/i18n/locale.detector.mjs' {
   import type { LocaleDetector } from '@nuxtjs/i18n/dist/runtime/composables/server'
 
   export const localeDetector: LocaleDetector
+}
+
+declare module '#internal/i18n-type-generation-options' {
+  export const dtsFile: string
 }
 
 declare module '#nuxt-i18n/logger' {
