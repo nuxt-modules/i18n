d44f37d09aa6a02945876ff27ca91b4119074b55
Bobbie Goede <bobbiegoede@gmail.com>
2025-04-24 13:03:28 +0200

    refactor: inline compat helper

--- src/runtime/compatibility.ts
+++ src/runtime/compatibility.ts
@@ -2,33 +2,15 @@
  * Utility functions to support both VueI18n and Composer instances
  */
 
-import { isRef } from 'vue'
-
 import type { Composer, I18n, VueI18n } from 'vue-i18n'
 
-function isI18nInstance(i18n: I18n | VueI18n | Composer): i18n is I18n {
-  return i18n != null && 'global' in i18n && 'mode' in i18n
-}
-
-function isComposer(target: I18n | VueI18n | Composer): target is Composer {
-  return target != null && !('__composer' in target) && 'locale' in target && isRef(target.locale)
-}
-
-export function isVueI18n(target: I18n | VueI18n | Composer): target is VueI18n {
-  return target != null && '__composer' in target
-}
-
 export function getI18nTarget(i18n: I18n | VueI18n | Composer) {
-  return isI18nInstance(i18n) ? i18n.global : i18n
+  return i18n != null && 'global' in i18n && 'mode' in i18n ? i18n.global : i18n
 }
 
 export function getComposer(i18n: I18n | VueI18n | Composer): Composer {
   const target = getI18nTarget(i18n)
-
-  if (isComposer(target)) return target
-  if (isVueI18n(target)) return target.__composer
-
-  return target
+  return '__composer' in target ? target.__composer : target
 }
 
 declare module 'vue-i18n' {
--- src/runtime/routing/i18n.ts
+++ src/runtime/routing/i18n.ts
@@ -1,6 +1,6 @@
 import { effectScope } from '#imports'
 import { assign } from '@intlify/shared'
-import { isVueI18n, getComposer } from '../compatibility'
+import { getComposer } from '../compatibility'
 
 import type { NuxtApp } from 'nuxt/app'
 import type { Composer, ComposerExtender, ExportedGlobalComposer, I18n, VueI18n, VueI18nExtender } from 'vue-i18n'
@@ -50,7 +50,7 @@ export function extendI18n(i18n: I18n, { extendComposer, extendComposerInstance 
     const globalComposer = getComposer(i18n)
     scope.run(() => {
       extendComposer(globalComposer)
-      if (i18n.mode === 'legacy' && isVueI18n(i18n.global)) {
+      if (i18n.mode === 'legacy' && '__composer' in i18n.global) {
         extendComposerInstance(i18n.global, getComposer(i18n.global))
       }
     })
