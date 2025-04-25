c792a3dd58774825322e5003cd87c38607430c94
Bobbie Goede <bobbiegoede@gmail.com>
2025-04-25 18:26:32 +0200

    perf: use compiler constant for tree-shaking

--- src/runtime/plugins/route-resolution-enhancement.ts
+++ src/runtime/plugins/route-resolution-enhancement.ts
@@ -4,7 +4,6 @@ import { useRouter } from 'vue-router'
 import { isString } from '@intlify/shared'
 
 import type { Locale } from 'vue-i18n'
-import type { I18nPublicRuntimeConfig } from '#internal-i18n-types'
 
 type ResolveParams = Parameters<import('#vue-router').Router['resolve']>
 
@@ -12,20 +11,19 @@ export default defineNuxtPlugin({
   name: 'i18n:route-resolution-enhancement',
   dependsOn: ['i18n:plugin'],
   setup() {
-    const nuxt = useNuxtApp()
-    const runtimeI18n = nuxt.$config.public.i18n as I18nPublicRuntimeConfig
-    if (!runtimeI18n.experimental.routeResolutionEnhancement) return
+    const nuxt = /*#__PURE__*/ useNuxtApp()
+    if (!__I18N_ROUTE_RESOLUTION__) return
 
     const ctx = nuxt._nuxtI18n
     const router = useRouter()
-    const implicit = runtimeI18n.experimental.routeResolutionEnhancement === 'implicit'
 
     /**
      * disable enhancement
      * - explicit mode without `locale`
      * - implicit mode with `locale: false`
      */
-    const disableEnhancement = (locale?: Locale | boolean) => (!implicit && locale == null) || locale === false
+    const disableEnhancement = (locale?: Locale | boolean) =>
+      (__I18N_ROUTE_RESOLUTION__ !== 'implicit' && locale == null) || locale === false
 
     const originalResolve = router.resolve.bind(router)
     router.resolve = (
