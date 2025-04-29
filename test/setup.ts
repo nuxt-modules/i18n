abebd27ffe1daec1cee956be1e72daf3b18f7959
Bobbie Goede <bobbiegoede@gmail.com>
2025-04-29 23:42:26 +0200

    test: cleanup global constant stub

--- test/setup.ts
+++ test/setup.ts
@@ -4,8 +4,9 @@ import { vi } from 'vitest'
 vi.stubGlobal('__I18N_CACHE__', false)
 vi.stubGlobal('__I18N_CACHE_LIFETIME__', -1)
 
-vi.stubGlobal('__DIFFERENT_DOMAINS__', false)
 vi.stubGlobal('__TRAILING_SLASH__', false)
+vi.stubGlobal('__DIFFERENT_DOMAINS__', false)
+vi.stubGlobal('__MULTI_DOMAIN_LOCALES__', false)
 
 vi.stubGlobal('__ROUTE_NAME_SEPARATOR__', '___')
 vi.stubGlobal('__ROUTE_NAME_DEFAULT_SUFFIX__', 'default')
