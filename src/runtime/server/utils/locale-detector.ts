fd9cb22df159f2351ff91d0b72e8c64e7a8262c4
Bobbie Goede <bobbiegoede@gmail.com>
2025-05-03 01:24:00 +0200

    feat: implement locale parsing from Accept-Language header and integrate with locale detection

--- src/runtime/server/utils/locale-detector.ts
+++ src/runtime/server/utils/locale-detector.ts
@@ -2,6 +2,9 @@ import { fetchMessages } from '../context'
 import { deepCopy } from '@intlify/shared'
 import { localeDetector } from '#internal/i18n/locale.detector.mjs'
 import { tryCookieLocale, tryHeaderLocale, tryQueryLocale } from '@intlify/h3'
+import { findBrowserLocale } from '#i18n-kit/routing'
+import { normalizedLocales } from '#internal/i18n/options.mjs'
+import { parseAcceptLanguage } from '../../browser'
 
 import type { H3Event } from 'h3'
 import type { CoreOptions, FallbackLocale, Locale } from '@intlify/core'
@@ -33,10 +36,15 @@ export function createDefaultLocaleDetector(opts: {
     }
 
     // try to get locale from header (`accept-header`)
-    const header = tryHeaderLocale(event, { lang: '' }) // disable locale default value with `lang` option
-    if (header) {
-      __DEBUG__ && console.log('locale detected from header', header.toString())
-      return header.toString() as string
+    const header = tryHeaderLocale(event, { lang: opts.defaultLocale }) // disable locale default value with `lang` option
+    const parsed = header && parseAcceptLanguage(header.toString())
+    if (header && parsed) {
+      const resolvedHeaderLocale = findBrowserLocale(
+        normalizedLocales.map(x => ({ code: x.code, language: x.language ?? x.code })),
+        parsed
+      )
+      __DEBUG__ && console.log('locale detected from header', resolvedHeaderLocale)
+      return resolvedHeaderLocale
     }
 
     // If the locale cannot be resolved up to this point, it is resolved with the value `locale` of the locale config passed to the function
