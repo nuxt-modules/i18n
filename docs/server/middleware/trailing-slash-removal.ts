faa5c376d2cf5ddf7876b98a1cbcfa57163a7529
Bobbie Goede <bobbiegoede@gmail.com>
2025-05-15 16:26:04 +0200

    docs: redirect to remove trailing slash

---
+++ docs/server/middleware/trailing-slash-removal.ts
@@ -0,0 +1,6 @@
+export default defineEventHandler(async event => {
+  // redirect to remove trailing slash
+  if (event.path.at(-1) === '/') {
+    await sendRedirect(event, event.path.slice(0, -1), 301)
+  }
+})
