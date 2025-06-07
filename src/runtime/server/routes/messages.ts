6c7aad4bc1fa4cfdfcabe5a50a49789aa9849969
Bobbie Goede <bobbiegoede@gmail.com>
2025-06-07 21:51:02 +0200

    fix: wrap cached endpoint for more control

--- src/runtime/server/routes/messages.ts
+++ src/runtime/server/routes/messages.ts
@@ -1,5 +1,5 @@
 import { deepCopy } from '@intlify/shared'
-import { defineCachedEventHandler } from 'nitropack/runtime'
+import { defineCachedEventHandler, defineCachedFunction } from 'nitropack/runtime'
 import { getRouterParam, createError, defineEventHandler } from 'h3'
 import { useI18nContext } from '../context'
 import { getMergedMessages } from '../utils/messages'
@@ -11,6 +11,8 @@ import type { H3Event } from 'h3'
  */
 const _messagesHandler = defineEventHandler(async (event: H3Event) => {
   const locale = getRouterParam(event, 'locale')
+
+  console.log(`Loading messages for locale: ${locale}`)
 
   if (!locale) {
     throw createError({ status: 400, message: 'Locale not specified.' })
@@ -27,20 +29,25 @@ const _messagesHandler = defineEventHandler(async (event: H3Event) => {
   return ctx.messages
 })
 
-/**
- * Load messages for the specified locale event parameter (cached)
- */
-const _messagesHandlerCached = defineCachedEventHandler(_messagesHandler, {
-  name: 'i18n:messages',
+const _cached = defineCachedFunction(_messagesHandler, {
+  name: 'i18n:messages-internal',
   maxAge: !__I18N_CACHE__ ? -1 : 60 * 60 * 24,
-  staleMaxAge: !__I18N_CACHE__ ? -1 : 60,
-  swr: __I18N_CACHE__,
   getKey: event => getRouterParam(event, 'locale') ?? 'null',
   shouldBypassCache(event) {
     const locale = getRouterParam(event, 'locale')
     if (locale == null) return false
     return !useI18nContext(event).localeConfigs?.[locale]?.cacheable
   }
+})
+
+/**
+ * Load messages for the specified locale event parameter (cached)
+ */
+const _messagesHandlerCached = defineCachedEventHandler(_cached, {
+  name: 'i18n:messages',
+  maxAge: !__I18N_CACHE__ ? -1 : 10,
+  swr: false,
+  getKey: event => getRouterParam(event, 'locale') ?? 'null'
 })
 
 /**
