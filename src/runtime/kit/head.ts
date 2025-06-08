a37a7b4b197bfac585a74a736d1471a5ec284543
Bobbie Goede <bobbiegoede@gmail.com>
2025-06-08 18:18:24 +0200

    fix: prevent adding multiple `x-default` alternate links

--- src/runtime/kit/head.ts
+++ src/runtime/kit/head.ts
@@ -134,7 +134,7 @@ function getHreflangLinks(options: HeadContext) {
     if (!link) continue
 
     links.push(link)
-    if (options.defaultLocale && options.defaultLocale === locale.code) {
+    if (options.defaultLocale && options.defaultLocale === locale.code && links[0].hreflang !== 'x-default') {
       links.unshift(
         strictSeo
           ? { rel: 'alternate', href: link.href, hreflang: 'x-default' }
