7f77f00c0546a8f92a57c0840c1726ce80a3accf
Bobbie Goede <bobbiegoede@gmail.com>
2025-10-28 09:20:28 +0100

    refactor: restructure module imports and dep setup

--- src/context.ts
+++ src/context.ts
@@ -4,7 +4,9 @@ import { dirname } from 'pathe'
 import { hash } from 'ohash'
 import type { Resolver } from '@nuxt/kit'
 import type { FileMeta, LocaleInfo, LocaleObject, NuxtI18nOptions } from './types'
-import type { Nuxt } from 'nuxt/schema'
+import type { Nuxt, NuxtConfigLayer } from 'nuxt/schema'
+import { getLayerI18n } from './utils'
+import { resolveI18nDir } from './layers'
 
 export interface I18nNuxtContext {
   resolver: Resolver
@@ -19,8 +21,10 @@ export interface I18nNuxtContext {
   runtimeDir: string
   fullStatic: boolean
   deploymentHash: string
+  i18nLayers: LayerWithI18n[]
 }
 
+type LayerWithI18n = { config: NuxtConfigLayer, i18n: Partial<NuxtI18nOptions>, i18nDir: string }
 const resolver = createResolver(import.meta.url)
 const distDir = dirname(fileURLToPath(import.meta.url))
 const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
@@ -28,12 +32,20 @@ const runtimeDir = fileURLToPath(new URL('./runtime', import.meta.url))
 export function createContext(userOptions: NuxtI18nOptions, nuxt: Nuxt): I18nNuxtContext {
   const options = userOptions as Required<NuxtI18nOptions>
 
+  const i18nLayers: LayerWithI18n[] = []
+  for (const l of nuxt.options._layers) {
+    const i18n = getLayerI18n(l)
+    if (!i18n) { continue }
+    i18nLayers.push({ config: l, i18n, i18nDir: resolveI18nDir(l, i18n) })
+  }
+
   return {
     options,
     resolver,
     userOptions,
     distDir,
     runtimeDir,
+    i18nLayers,
     localeInfo: undefined!,
     localeCodes: undefined!,
     normalizedLocales: undefined!,
