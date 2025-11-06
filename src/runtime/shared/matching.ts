723eea56bc3a17144139fdcaa7b9d1806c77a04e
ziyoonee <39004948+ziyoonee@users.noreply.github.com>
2025-11-07 08:10:30 +0900

    fix: skip localized redirection on nuxt error routes (#3871)
    Co-authored-by: ziyoonee <ziyoonee@users.noreply.github.com>
    Co-authored-by: Bobbie Goede <bobbiegoede@gmail.com>

--- src/runtime/shared/matching.ts
+++ src/runtime/shared/matching.ts
@@ -18,6 +18,10 @@ const getI18nPathToI18nPath = (path: string, locale: string) => {
 
 export function isExistingNuxtRoute(path: string) {
   if (path === '') { return }
+  // TODO: path should not have base url - check if base url is stripped earlier
+  // skip nuxt error route - this path is hardcoded within nitro context code in nuxt
+  if (path.endsWith('/__nuxt_error')) { return }
+
   const resolvedMatch = matcher.resolve({ path }, { path: '/', name: '', matched: [], params: {}, meta: {} })
 
   return resolvedMatch.matched.length > 0 ? resolvedMatch : undefined
