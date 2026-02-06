b32d58bf9add0f581413e8879780151ccc8572d8
Bobbie Goede <bobbiegoede@gmail.com>
2026-02-06 17:21:34 +0100

    ci: fix release flow (#3924)

--- scripts/release-edge.sh
+++ scripts/release-edge.sh
@@ -11,14 +11,6 @@ pnpm jiti ./scripts/bump-edge
 # Resolve pnpm
 pnpm i --frozen-lockfile=false
 
-# Update token
-if [[ ! -z ${NPM_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
-  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-  echo "always-auth=true" >> ~/.npmrc
-  npm whoami
-fi
-
 # Release package
 echo "⚡ Publishing edge version"
-npx npm@8.17.0 publish --tag latest --access public --tolerate-republish
+pnpm publish --tag latest --access public --provenance --no-git-checks
--- scripts/release.sh
+++ scripts/release.sh
@@ -8,14 +8,6 @@ git restore --source=HEAD --staged --worktree -- package.json pnpm-lock.yaml
 # Resolve pnpm
 pnpm i --frozen-lockfile=false
 
-# Update token
-if [[ ! -z ${NPM_TOKEN} ]] ; then
-  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> ~/.npmrc
-  echo "registry=https://registry.npmjs.org/" >> ~/.npmrc
-  echo "always-auth=true" >> ~/.npmrc
-  npm whoami
-fi
-
 # Release package
 echo "⚡ Publishing with tag latest"
-npx npm@8.17.0 publish --tag latest --access public --tolerate-republish
+pnpm publish --tag latest --access public --provenance --no-git-checks
