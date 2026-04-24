b8e8b551818f1820d4128cbca35011fdd139bd9c
Bobbie Goede <bobbiegoede@gmail.com>
2026-04-24 14:13:38 +0200

    fix: incorrect route group server redirection during ssg

---
+++ specs/fixtures/issues/3929/nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['@nuxtjs/i18n'],
+  i18n: {
+    locales: ['en', 'fr'],
+    defaultLocale: 'en',
+    detectBrowserLanguage: {
+      redirectOn: 'no prefix'
+    }
+  }
+})
