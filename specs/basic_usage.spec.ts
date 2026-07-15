885f604157b5f6718eff936bf24b4f01492540a9
Bobbie Goede <bobbiegoede@gmail.com>
2026-07-15 09:38:18 +0200

    test: merge seo specs (#4038)

--- specs/basic_usage.spec.ts
+++ specs/basic_usage.spec.ts
@@ -491,6 +491,33 @@ describe('basic usage', async () => {
       )
     })
 
+    test('respects `experimental.alternateLinkCanonicalQueries: false`', async () => {
+      await setServerRuntimeConfig({
+        public: {
+          i18n: { experimental: { alternateLinkCanonicalQueries: false } }
+        }
+      })
+
+      // head tags - alt links are updated server side
+      const product1Html = await $fetch('/products/big-chair?test=123&canonical=123')
+      const product1Dom = await getDom(product1Html)
+      expect(await product1Dom.locator('link[id=i18n-alt-nl]')?.getAttribute('href')).toEqual(
+        'http://localhost:3000/nl/products/grote-stoel?test=123&canonical=123'
+      )
+      expect(await product1Dom.locator('#switch-locale-path-link-nl')?.getAttribute('href')).toEqual(
+        '/nl/products/grote-stoel?test=123&canonical=123'
+      )
+
+      const product2Html = await $fetch('/nl/products/rode-mok?test=123&canonical=123')
+      const product2dom = await getDom(product2Html)
+      expect(await product2dom.locator('link[id=i18n-alt-en]')?.getAttribute('href')).toEqual(
+        'http://localhost:3000/products/red-mug?test=123&canonical=123'
+      )
+      expect(await product2dom.locator('#switch-locale-path-link-en')?.getAttribute('href')).toEqual(
+        '/products/red-mug?test=123&canonical=123'
+      )
+    })
+
     test('dynamic parameters rendered correctly during SSR', async () => {
       // head tags - alt links are updated server side
       const product1Html = await $fetch('/products/big-chair')
